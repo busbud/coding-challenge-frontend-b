637d4468569f134fabe4a3f1d4fdfba855fe69a8
Nora <top.nora@gmail.com>
2016-01-31 23:49:40 -0500

    v0 ok

--- js/components/Home.js
+++ js/components/Home.js
@@ -30,10 +30,10 @@ var Home = React.createClass({
         const store = this.props.store;
         return (
             <div className="wrapper">
-                <div className="header row align-middle">
+                <div className="header row align-middle align-justify">
                     <LanguageSelector currentPath={this.props.location.pathname}/>
-                    <div className="header_busbud-logo"><a href="https://www.busbud.com/en" target="_blank"><img src="https://busbud-pubweb-assets.global.ssl.fastly.net/images/logos/fc7ed21.logo-post-60@2x.png" alt="busbud logo"/></a></div>
-                    <h1 className="header_title">Front-end coding challenge</h1>
+                    <div className="columns small-9 medium-3 header_busbud-logo"><a href="https://www.busbud.com/en" target="_blank"><img src="https://busbud-pubweb-assets.global.ssl.fastly.net/images/logos/fc7ed21.logo-post-60@2x.png" alt="busbud logo"/></a></div>
+                    <h1 className="columns header_title">Front-end coding challenge</h1>
                 </div>
                 {this.getChildrenWithStore()}
             </div>
--- js/components/Landing.js
+++ js/components/Landing.js
@@ -1,11 +1,85 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+const gallery = [
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/569ddee1488dd.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/569dded5616f7.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/569ddec9e5107.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/569ddec2b96e4.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a404a49ddb5.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a404bb244e3.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a404b219d7d.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a404ad503a2.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a2bb7a46b83.jpg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a2bb62bec09.jpg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a6b6d20ec08.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a6b6c35209f.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a6b6b451cc1.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a6b6b003379.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a6b6aa76e1d.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/56a6b6a03f659.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/569984c384a8d.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/569984b43d4b8.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/5699848d23183.jpeg"
+    },
+    {
+        img:"http://igloofest.ca/public/app/uploads/images/569984d1224b3.jpeg"
+    }
+];
+
 var Landing = React.createClass({
     render() {
         return (
             <div>
-               <p>Landing page</p>
+                <div className="home-section">
+                    <div className="home-section_ticket">
+                        <img className="home-section_igloofestlogo" src="http://igloofest.ca/public/app/uploads/images/5661d13eecfee.png" alt="igloofest logo"/>
+                        <Link className="home-section_link" to="/fr/departures/dr5reg/f25dvk/2016-02-05">GET YOUR BUS TICKET NOW!</Link>
+                    </div>
+                    <div className="home-section_busbudlogo"><img src="https://busbud-pubweb-assets.global.ssl.fastly.net/images/logos/fc7ed21.logo-post-60@2x.png" alt="busbud logo"/></div>
+                </div>
+                <ul className="gallery">
+                    {gallery.map((tile,index) => (
+                        <li key={index} className="gallery_item"><img src={tile.img} /></li>
+                    ))}
+                </ul>
             </div>
         )
     }
