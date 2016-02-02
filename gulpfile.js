578a38516106b940308b14c84da86375372c1a1a
Nora <top.nora@gmail.com>
2016-02-01 22:39:48 -0500

    added minify js and compressed css + gulp production task

--- gulpfile.js
+++ gulpfile.js
@@ -14,86 +14,90 @@ var buffer = require('vinyl-buffer');
 
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-var historyApiFallback = require('connect-history-api-fallback')
+var historyApiFallback = require('connect-history-api-fallback');
 
 
 /*
-  Styles Task
-*/
+ Styles Task
+ */
 
-gulp.task('styles',function() {
-  // move over fonts
+gulp.task('styles', function () {
+    // move over fonts
 
-  // Compiles CSS
-  gulp.src('scss/style.scss')
-    .pipe(sass({includePaths: ['node_modules/foundation-sites/scss']}))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./build/css/'))
-    .pipe(reload({stream:true}))
+    // Compiles CSS
+    gulp.src('scss/style.scss')
+        .pipe(sass({outputStyle: 'compressed', includePaths: ['node_modules/foundation-sites/scss']}))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./build/css/'))
+        .pipe(reload({stream: true}))
 });
 
 /*
-  Browser Sync
-*/
-gulp.task('browser-sync', function() {
+ Browser Sync
+ */
+gulp.task('browser-sync', function () {
     browserSync({
         // we need to disable clicks and forms for when we test multiple rooms
-        server : {},
-        middleware : [ historyApiFallback() ],
+        server: {},
+        middleware: [historyApiFallback()],
         ghostMode: false
     });
 });
 
 function handleErrors() {
-  var args = Array.prototype.slice.call(arguments);
-  notify.onError({
-    title: 'Compile Error',
-    message: '<%= error.message %>'
-  }).apply(this, args);
-  this.emit('end'); // Keep gulp from hanging on this task
+    var args = Array.prototype.slice.call(arguments);
+    notify.onError({
+        title: 'Compile Error',
+        message: '<%= error.message %>'
+    }).apply(this, args);
+    this.emit('end'); // Keep gulp from hanging on this task
 }
 
 function buildScript(file, watch) {
-  
-  var props = {
-    entries: ['./js/' + file],
-    debug : true,
-    transform:  [babelify.configure({stage : 0 })]
-  };
 
-  // watchify() if watch requested, otherwise run browserify() once 
-  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+    var props = {
+        entries: ['./js/' + file],
+        debug: true,
+        transform: [babelify.configure({stage: 0})]
+    };
 
-  function rebundle() {
-    var stream = bundler.bundle();
-    return stream
-      .on('error', handleErrors)
-      .pipe(source(file))
-      .pipe(gulp.dest('./build/js/'))
-      // If you also want to uglify it
-      // .pipe(buffer())
-      // .pipe(uglify())
-      // .pipe(rename('app.min.js'))
-      // .pipe(gulp.dest('./build'))
-      .pipe(reload({stream:true}))
-  }
+    // watchify() if watch requested, otherwise run browserify() once
+    var bundler = watch ? watchify(browserify(props)) : browserify(props);
 
-  // listen for an update and run rebundle
-  bundler.on('update', function() {
-    rebundle();
-    gutil.log('Rebundle...');
-  });
+    function rebundle() {
+        var stream = bundler.bundle();
+        return stream
+            .on('error', handleErrors)
+            .pipe(source(file))
+            // If you also want to uglify it
+            .pipe(buffer())
+            .pipe(uglify())
+            .pipe(rename('app.min.js'))
+            .pipe(gulp.dest('./build/js/'))
+            .pipe(reload({stream: true}))
+    }
 
-  // run it once the first time buildScript is called
-  return rebundle();
+    // listen for an update and run rebundle
+    bundler.on('update', function () {
+        rebundle();
+        gutil.log('Rebundle...');
+    });
+
+    // run it once the first time buildScript is called
+    return rebundle();
 }
 
-gulp.task('scripts', function() {
-  return buildScript('main.js', false); // this will once run once because we set watch to false
+gulp.task('scripts', function () {
+    return buildScript('main.js', false); // this will once run once because we set watch to false
 });
 
 // run 'scripts' task first, then watch for future changes
-gulp.task('default', ['styles','scripts','browser-sync'], function() {
-  gulp.watch('scss/**/*', ['styles']); // gulp watch for stylus changes
-  return buildScript('main.js', true); // browserify watch for JS changes
+gulp.task('default', ['styles', 'scripts', 'browser-sync'], function () {
+    gulp.watch('scss/**/*', ['styles']); // gulp watch for stylus changes
+    return buildScript('main.js', true); // browserify watch for JS changes
+});
+
+// production
+gulp.task('production',['styles', 'scripts'],function() {
+    return buildScript('main.js', false); // this will once run once because we set watch to false
 });
