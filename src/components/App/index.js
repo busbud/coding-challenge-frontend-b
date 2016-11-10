61690a66b39cecd1a22d198e9398e533c9135e55
Ben Wiley <samushighwind@gmail.com>
2016-11-09 21:41:31 -0500

    App styles updated; image included.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,19 @@
 
 import React, { PropTypes } from 'react';
-import styles from './styles.scss';
 
-function App() {
-  return (
-    <div>
-      <h1 className={styles.header}>Hello World</h1>
-      <div className={styles.content}>
-        Welcome to the app.
-      </div>
-    </div>
-  );
-}
+import styles from './styles.scss';
+import '../../shared-styles/globals.scss';
+
+
+const App = () => (
+  <div className={styles.content}>
+    <img
+      src="https://cloud.githubusercontent.com/assets/1574577/12971188/13471bd0-d066-11e5-8729-f0ca5375752e.png"
+      alt="Osheaga Festival Musique et Arts"
+    />
+    <h1 className={styles.header}>Hello World</h1>
+    <p>Welcome to the app.</p>
+  </div>
+);
 
 export default App;
