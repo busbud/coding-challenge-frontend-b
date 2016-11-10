e60f63d911ab56133a3109049c10f111ab57af1d
Ben Wiley <samushighwind@gmail.com>
2016-11-10 15:37:14 -0500

    Banner image included locally in project.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,7 +9,7 @@ const App = () => (
   <div className={styles.content}>
     <img
       className={styles.banner}
-      src="https://cloud.githubusercontent.com/assets/1574577/12971188/13471bd0-d066-11e5-8729-f0ca5375752e.png"
+      src="/img/banner.png"
       alt="Osheaga Festival Musique et Arts"
     />
     <Departures />
