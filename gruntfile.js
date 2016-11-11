90c27b6ae32a2d253aee1e878b77ceb736dd43b4
Callum May <callumdmay@gmail.com>
2016-11-11 00:49:09 -0500

    Fix gruntfile formatting

--- gruntfile.js
+++ gruntfile.js
@@ -18,43 +18,44 @@ module.exports = function(grunt) {
                 src: ['**/*.eot', '**/*.svg', '**/*.ttf', '**/*.woff', '**/*.woff2'],
                 dest: './dist/'
             },
-            node_modules:{
-              expand: true,
-              cwd: './node_modules/',
-              flatten: true,
-              src: ['angular/angular.min.js', 'angular-route/angular-route.min.js',
-              'moment/min/moment.min.js', 'bootstrap/dist/css/bootstrap.min.css',
-              'angular-ui-swiper/dist/angular-ui-swiper.js'],
-              dest: './dist/'
+            node_modules: {
+                expand: true,
+                cwd: './node_modules/',
+                flatten: true,
+                src: ['angular/angular.min.js', 'angular-route/angular-route.min.js',
+                    'moment/min/moment.min.js', 'bootstrap/dist/css/bootstrap.min.css',
+                    'angular-ui-swiper/dist/angular-ui-swiper.js'
+                ],
+                dest: './dist/'
             },
 
             views: {
-              expand: true,
-              cwd: './app/',
-              src: ['**/*.html'],
-              flatten: true,
-              dest: './dist/'
+                expand: true,
+                cwd: './app/',
+                src: ['**/*.html'],
+                flatten: true,
+                dest: './dist/'
             },
 
             components: {
-              expand: true,
-              cwd: './app/',
-              src: ['components'],
-              flatten: true,
-              dest: './dist/'
+                expand: true,
+                cwd: './app/',
+                src: ['components'],
+                flatten: true,
+                dest: './dist/'
             }
         },
         babel: {
-        options: {
-            sourceMap: true,
-            presets: ['es2015', 'react']
-        },
-        dist: {
-            files: {
-                'dist/app.js': 'src/app.js'
+            options: {
+                sourceMap: true,
+                presets: ['es2015', 'react']
+            },
+            dist: {
+                files: {
+                    "dist/app.js": "app/**/*.js"
+                }
             }
         }
-    }
 
     });
 
@@ -69,5 +70,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('load-grunt-tasks');
 
     //register grunt default task
-    grunt.registerTask('test', ['eslint', 'clean', 'concat', 'copy', 'uglify']);
+    grunt.registerTask('test', ['eslint', 'clean', 'copy']);
+    grunt.registerTask('default', ['eslint', 'clean', 'copy']);
 }
