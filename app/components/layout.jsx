95ed77e81f9162bbd960379e4bcedc52846ba6f6
Callum May <callumdmay@gmail.com>
2016-11-11 23:05:06 -0500

    Improve layout

--- app/components/layout.jsx
+++ app/components/layout.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
 
 export default class Layout extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        <header>
-        </header>
-        <div className="app-content">{this.props.children}</div>
-        <footer>
-        </footer>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app-container">
+                <header>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-1 col-md-offset-1" id="pageName">
+                                Callumbud
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <div className="app-content">{this.props.children}</div>
+                <footer></footer>
+            </div>
+        );
+    }
 }
