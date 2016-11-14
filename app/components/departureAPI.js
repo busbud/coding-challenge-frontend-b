16aa9c0634da50eb3ddb703f5399348b0c04ebdd
Callum May <callumdmay@gmail.com>
2016-11-13 23:51:37 -0500

    Clean api url string

--- app/components/departureAPI.js
+++ app/components/departureAPI.js
@@ -6,7 +6,7 @@ class DepartureAPI {
 
       fetchDeparture(origin, destination, date){
 
-        return fetch('https://napi.busbud.com/x-departures/' + origin + '/' + destination + '/' + date.toISOString().slice(0, 10), {
+        return fetch(`https://napi.busbud.com/x-departures/${origin}/${destination}/${date.toISOString().slice(0, 10)}`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
