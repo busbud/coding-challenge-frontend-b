c1771276e99cf46b863eebe5fe37ee658c1ab1e2
Callum May <callumdmay@gmail.com>
2016-11-15 00:05:27 -0500

    Add process variable to heroku deploy

--- app/components/departureAPI.js
+++ app/components/departureAPI.js
@@ -10,7 +10,7 @@ class DepartureAPI {
             method: 'GET',
             headers: {
                 'Accept': 'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
-                'X-Busbud-Token': ''
+                'X-Busbud-Token': process.env.busbud_token
             }
         });
       }
