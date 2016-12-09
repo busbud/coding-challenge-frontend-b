dec93e8288b89eb960ee97f7b99bf145414282e6
RB <rombevillard@gmail.com>
2016-12-09 10:42:14 +0100

    heroku assigns PORT dynamically in process

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var router = require('./router');
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 app.set('view engine', 'pug');
 
