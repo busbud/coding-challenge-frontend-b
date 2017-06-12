3d154bce30eea1e96a5ae3c8566208f6bb0f751b
tlabna <tlabna@users.noreply.github.com>
2017-06-11 23:43:26 -0400

    chore: Update webpack.config and package.json for production build
    1. Add express package to serve app on heroku
    2. Update package.json with scripts to run building for production
    3. Update webpack.config to set node_env to production and uglify js code

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var path = require('path');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+var config = {
 	entry: './app/index.js',
 	output: {
 		filename: 'index_bundle.js',
@@ -48,3 +49,24 @@ module.exports = {
 			{template: 'app/index.html'}
 		)]
 };
+
+// We set NODE_ENV to production in package.json 
+// so that we can check it's state in webpack.config.js
+if (process.env.NODE_ENV === 'production') {
+	config.plugins.push(
+		// Create new instace of webpack.DefinePlugin
+		new webpack.DefinePlugin({
+			// Set property on process.env
+			'process.env': {
+				// We do this so we set NODE_ENV to production
+				// in our compiled code that webpack will compile
+				'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+			}
+		}),
+		// Add new instance of webpack.optimize
+		// Call UglifyJsPlugin to minify our code
+		new webpack.optimize.UglifyJsPlugin()
+	);
+}
+
+module.exports = config;
