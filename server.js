a3f999c6c9e1fba74e23872be400de073a51a5e6
Marc-Andre Beauchamp <marcandre_beauchamp@mcafee.com>
2017-08-11 16:23:01 -0400

    head => brick wall

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(webpackDevMiddleware(compiler, {
   historyApiFallback: true,
 }));
  
-const server = app.listen(3000, function() {
+const server = app.listen(5000, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
