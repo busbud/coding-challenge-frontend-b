437e96669f5e3a3ffad0950c03c27cbdd4d3186c
Marc-Andre Beauchamp <marcandre_beauchamp@mcafee.com>
2017-08-16 10:26:27 -0400

    remove unused

--- src/components/App.js
+++ src/components/App.js
@@ -76,7 +76,6 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<MuiThemeProvider>
 				<div>
 					<div id="header">
 						<img src="https://cloud.githubusercontent.com/assets/1574577/12971188/13471bd0-d066-11e5-8729-f0ca5375752e.png"/>
@@ -88,7 +87,6 @@ class App extends React.Component {
 							searchHandler={this.searchHandler}
 						/>
 					</div>
-			</MuiThemeProvider>
 		)
 	}
 };
