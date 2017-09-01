e283a4968bb1f26cbb7c8a69ebd9afc464ed7468
Victor Mus <sdwvit@users.noreply.github.com>
2017-09-01 22:01:15 +0300

    Update onDateChange.js

--- src/actions/sideEffects/onDateChange.js
+++ src/actions/sideEffects/onDateChange.js
@@ -4,8 +4,8 @@ export default (payload) => (dispatch, getState) => {
 	const state = getState().app;
 	if (state.date !== payload && state.complete) {
 		dispatch(thunks[types.fetchTickets]({
-			index: 0,
 			...getState(),
+			index: 0,
 		}));
 	}
 };
