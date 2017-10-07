e72bca9434b4e9b4e77e793dfcd9f5285f63e614
LQ <lq@long-quan.com>
2017-10-07 13:14:14 -0400

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
