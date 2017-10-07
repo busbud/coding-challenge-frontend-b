eee3b15f6e4cf2ae66e1202520a383bc80e1a94e
LQ <lq@long-quan.com>
2017-10-07 11:39:05 -0400

    setup react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="header-content">
+            <h1 className="App-title">BusBud Front-end Challenge</h1>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
