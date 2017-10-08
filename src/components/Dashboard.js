0e2feef7afbb5adc4fdf0f7bb1a9829b0f90f498
LQ <lq@long-quan.com>
2017-10-08 16:46:43 -0400

    fixed classname error which led to css not working

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,10 +4,10 @@ import './Dashboard.css';
 class Dashboard extends Component {
   render() {
     return (
-      <div className="Dashboards">
-        <header className="Dashboards-header">
+      <div className="Dashboard">
+        <header className="Dashboard-header">
           <div className="header-content">
-            <h1 className="Dashboards-title">BusBud Front-end Challenge</h1>
+            <h1 className="Dashboard-title">BusBud Front-end Challenge</h1>
           </div>
         </header>
       </div>
