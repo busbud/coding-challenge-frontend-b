c5bfbaf168327846ba3ce16233f4f04ebe0e533f
LQ <lq@long-quan.com>
2017-10-16 01:22:44 -0400

    access to api now

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 import '../styles/base.scss';
 
@@ -8,18 +9,30 @@ class SearchBtn extends Component {
   constructor(props) {
     super(props)
 
-    this.state({ 
-    })
+    this.state = {}
   }
 
   render() {
-    const searchBtn = (<Button bsSize="large" bsStyle="warning">Search</Button>);
 
     return (
       <div className="search-btn">
-        {searchBtn}
+        {<Button bsSize="large" bsStyle="warning" onClick={this.getDepartures}>Search</Button>}
       </div>
     )
+  }
+
+  getDepartures() {
+    return axios.get('https://napi.busbud.com/x-departures/dr5reg/f25dvk/2018-0802', 
+    {
+      'headers': {
+        'Accept': 'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
+        'X-Busbud-Token': 'PARTNER_JSWsVZQcS_KzxNRzGtIt1A'
+      }
+    }).then(response => {
+      console.log(response.data);
+    }).catch((error) => {
+      console.log(error)
+    });
   }
 }
 
