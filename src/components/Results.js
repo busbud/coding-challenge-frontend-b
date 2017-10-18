e7e81df47f0424eab213729a09c4ad06087c27ea
LQ <lq@long-quan.com>
2017-10-18 01:35:29 -0400

    (wip) fetching data not working :\

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,45 +1,40 @@
 import React, { Component } from 'react';
 
 import '../styles/base.scss';
-import SearchBtn from './search';
-import Placeholder from '../img/500x500.png';
 
 class Results extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      displayResults: false,
-      departures: {}
-    }
-  }
-
   render() {
 
-    return (
-      <main className="results-container">
-        <h1 className="result-header white-text">Here is what we found</h1>
-
+    if (this.props.displayResults) {
+      return (
+        <main className="results-container">
+          <h1 className="result-header white-text">Here is what we found</h1>
+  
+          { this.props.departures.map((departure) => 
             <article className="result-card">
-            <div className="column operator-img">
-              <img src={ Placeholder }alt={"placeholder"} className="operator"/>
-              <p className="operator-name">Greyhound</p>
-            </div>
-            <div className="column travel-info">
-              <h2 className="travel-title">Departure</h2>
-              <p className="departure"><span className="time">10:00</span> - NYC Train Station</p>
-              <h2 className="travel-title">Arrival</h2>          
-              <p className="arrival"><span className="time">23:59</span> - MTL Train Station</p>
-              <hr/>          
-              <p className="travel-time"><span className="duration">Total trip duration: </span> 8 hours and 59 minutes</p>
-            </div>
-            <div className="column price-info">
-                <p className="price">50$ <span className="currency">CAD</span></p>
-            </div>
-          </article>
-      </main>
-    );
+              <div className="column operator-img">
+                <img src={ departure.operatorImg }alt={"operator"} className="operator"/>
+                <p className="operator-name">{ departure.operator }</p>
+              </div>
+              <div className="column travel-info">
+                <h2 className="travel-title">Departure</h2>
+                <p className="departure"><span className="time">{ departure.departureTime }</span> - { departure.departureLocation }</p>
+                <h2 className="travel-title">Arrival</h2>          
+                <p className="arrival"><span className="time">{ departure.arrivalTime }</span> - { departure.arrivalLocation }</p>
+                <hr/>          
+                <p className="travel-time"><span className="duration">Total trip duration: </span> 8 hours and 59 minutes</p>
+              </div>
+              <div className="column price-info">
+                  <p className="price">{ departure.price }$ <span className="currency">{ departure.currency }</span></p>
+              </div>
+            </article>
+          )}
+        </main>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
--- src/components/search.js
+++ src/components/search.js
@@ -1,20 +1,59 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
+import moment from 'moment';
+
+import Results from './results';
 
 import '../styles/base.scss';
 
 class SearchBtn extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
     this.state = {
       displayResults: false,
-      departures: {}
+      fetchComplete: false,
+      departures: []
     };
   }
 
+  _getDepartures(data) {
+    if (this.state.displayResults === false && this.state.fetchComplete === true) {
+      let dataHolder = {};
+      let completeHolder = data.complete;
+
+      for (let departure of data.departures) {
+        dataHolder.operatorImg        = data.operators.find((operator) => operator.id === departure.operator_id).logo_url;
+        dataHolder.operator           = data.operators.find((operator)=> operator.id === departure.operator_id).name;
+        dataHolder.departureTime      = moment(data.departure_time).format('LT');
+        dataHolder.departureLocation  = data.locations.find((location) => location.id === departure.origin_location_id).name;
+        dataHolder.arrivalTime        = moment(data.arrival_time).format('LT');
+        dataHolder.arrivalLocation    = data.locations.find((location) => departure.destination_location_id).name;
+        dataHolder.price              = departure.prices.total/100;
+        dataHolder.currency           = departure.prices.currency;
+      }
+      this.setState({
+        departures: dataHolder,
+        fetchComplete: completeHolder
+      });
+      this._getDepartures(this.state.departures);
+    } else {
+      this.search();
+      console.log(this.search())
+    }
+
+  }
+
   search() {
+    if (this.state.displayResults === false && this.state.departures.length === 0) {
+      return (this._fetchResults());
+      this._fetchResults();
+    }
+  }
+
+  _fetchResults() {
     return (dispatch) => {
       return axios.get('https://napi.busbud.com/x-departures/dr5reg/f25dvk/2018-08-02', 
       {
@@ -23,11 +62,10 @@ class SearchBtn extends Component {
           'X-Busbud-Token': 'PARTNER_JSWsVZQcS_KzxNRzGtIt1A'
         }
       }).then((response => {
-        dispatch(this._displayResults(response.data));
+        dispatch(this._getDepartures(response.data));
       })).then(console.log(this.state.departures), console.log(this.state.displayResults))
       .catch((error) => {
         console.log(error);
-        this.setState({displayResults: false});
       });
     }
   }
@@ -35,10 +73,13 @@ class SearchBtn extends Component {
   _displayResults(data) {
     if (this.state.displayResults === false || this.state.departures.complete === true) {
       this.setState({
-        displayResults: true,
-        departures: data
+        displayResults: true
       })
     }
+
+    return (
+        <Results departures= { this.state.departures } />
+    )
   }
 
   render() {
