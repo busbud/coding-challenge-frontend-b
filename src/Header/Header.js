231f8d7d24536bffbb1bf5c55bbd81548677a2e2
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2017-10-29 23:41:57 +0100

    feat(Header): add Busbud logo and favicon

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Image } from 'semantic-ui-react';
 import './Header.css';
+import logo from '../logo.svg';
 
 class Header extends Component {
   render() {
     return (
       <Grid.Column color="blue">
-        <h1 className="Header-title">{this.props.title}</h1>
+        <h1 className="Header-title">
+          <Image
+            src={logo}
+            alt="Busbud"
+            className="Header-logo"
+            avatar
+            verticalAlign="top"
+          />
+          <span>{this.props.title}</span>
+        </h1>
       </Grid.Column>
     );
   }
