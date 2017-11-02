1a7677ff939f406e484035665d0fd9e7ea0549a5
slater6 <slaters@gmail.com>
2017-11-01 23:10:11 -0400

    Setup Redux Actions

--- src/actions/departures.js
+++ src/actions/departures.js
@@ -1,8 +1,22 @@
-export const INITIALIZE_DEPARTURE_SEARCH = 'INITIALIZE_DEPARTURE_SEARCH'
+import Busbud from '../services/busbud'
+
+const busbud = new Busbud();
+
+export const LOAD_DEPARTURE_DATA = 'LOAD_DEPARTURE_DATA'
+
 
 export const getDepartures = (originHash,destinationHash,outboundDate) => {
     
         return (dispatch) => {
-           
+           busbud.getDepartures(originHash,destinationHash,outboundDate)
+           .then( response => {
+               
+                console.log(response['data'])
+
+                dispatch({
+                    type:LOAD_DEPARTURE_DATA,
+                    payload: response['data']
+                })
+           }).catch()
         } 
     }
