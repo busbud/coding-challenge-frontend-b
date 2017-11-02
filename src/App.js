2b082164376c3f441439f0cd9829d843b3514d2d
slater6 <slaters@gmail.com>
2017-11-01 23:10:52 -0400

    Setup a working test prototype

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Departures from './components/departures'
+import {
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Departures></Departures>
+      <Grid>
+        <Row className="show-grid">
+          <Col mdOffset={2} md={8}>
+            <Departures></Departures>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
--- src/components/departures.js
+++ src/components/departures.js
@@ -1,13 +1,38 @@
 import React,{Component} from 'react'
 import {connect} from 'react-redux'
+import {getDepartures} from '../actions'
+import {
+    ListGroup,
+    ListGroupItem
+} from 'react-bootstrap'
 
 
 class Departures extends Component{
+    componentDidMount(){
+        this.props.getDepartures('dr5reg','f25dvk','2018-08-02')
+    }
+    
     render(){
         return(
-            <h1>Hello!</h1>
+            <ListGroup>
+                {
+                    this.props.departures.departures.map(
+                        departure => (
+                            <ListGroupItem>{departure.id}</ListGroupItem>
+                        )
+                    )
+                }
+               
+            </ListGroup>
         )
     }
 }
 
-export default Departures
+export default connect(
+    (state) => ({
+        departures : state.departures
+    }),
+    {
+      getDepartures
+    }
+  )(Departures)
