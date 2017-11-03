e36db7daa8023901e207bed9c2df46592ddc452c
Clément Guillou <cguillou@wanderbrief.com>
2017-11-03 14:08:56 +0000

    VueJS base project

---
+++ src/components/DeparturesListing/DeparturesListing.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+
+	section(v-show="departures.length > 1").departures-listing
+
+		article(v-for="departure in departures").departures-listing__departure
+			h5.departure__class {{ departure.class | titleCase }}
+			span.departure__price {{ departure.prices.total | price }}
+			//
+				h4.departure__location {{ location | titleCase }}
+				h5.departure__class {{ departure.class | titleCase }}
+				span.departure__departure-time
+				span.departure__arrival-time
+				span.departure__price {{ departure.prices.total | price }}
+
+
+</template>
+
+<script src="./DeparturesListing.js"></script>
+
---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        
+        path: path.resolve(__dirname, './public/dist'),
+        publicPath: '/dist/',
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                exclude: /src/,
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+
+            'src': path.resolve(__dirname, './src'),
+            'assets': path.resolve(__dirname, './public/dist/assets'),
+            'pages': path.resolve(__dirname, './src/pages'),
+            'components': path.resolve(__dirname, './src/components'),
+            'stores': path.resolve(__dirname, './src/stores'),
+            'vue$': 'vue/dist/vue.common.js'
+        },
+        modules: [
+  
+            'node_modules',
+            './vendors'
+        ]
+    },
+    devServer: {
+
+        historyApiFallback: true,
+        noInfo: true,
+        contentBase: './public',
+    },
+    performance: {
+
+        hints: false
+    },
+    devtool: '#eval-source-map'
+}
+
+
+if (process.env.NODE_ENV === 'production') {
+
+    module.exports.devtool = '#source-map'
+
+    module.exports.plugins = (module.exports.plugins || []).concat([
+    
+        new webpack.DefinePlugin({
+            
+            'process.env': {
+
+                NODE_ENV: '"production"'
+            }
+        }),
+        
+        new webpack.optimize.UglifyJsPlugin({
+            
+            sourceMap: false,
+            compress: {
+
+                warnings: false
+            }
+        }),
+
+        new webpack.LoaderOptionsPlugin({
+            
+            minimize: true,
+            compress: {
+
+                warnings: false
+            }
+        })
+    ])
+}
+
