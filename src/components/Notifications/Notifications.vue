598bbce30239a3c481b7cfbce6f168918aa8296e
Clément Guillou <cguillou@wanderbrief.com>
2017-11-03 14:03:11 +0000

    Add Notifications Component

---
+++ src/components/Notifications/Notifications.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+
+	div(v-show="messages.length").notifications
+
+		div.notifications-wrapper.small
+
+			div(v-for="message in messages", :class="'notification ' + message.type") {{ message.text }}
+
+</template>
+
+<script src="./Notifications.js"></script>
+
