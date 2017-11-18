4f7432054429d793807729ee47f0c65ce548c738
Edmon <comlaterra@gmail.com>
2017-11-18 18:53:44 +0100

    Recurrence on non completed requests,

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -11,16 +11,38 @@ export function requestResults(results){
 }
 
 export function fetchResults(results){
+
+	let searchResults
+
+	let request = function () {
+    // dispatch(requestResults(results))
+    // @TODO The url must come from a config file
+    return fetch(`http://localhost:8080/busbud`)
+		.then(
+			response => response.json(),
+			error => console.log('An error occurred.', error)
+		)
+		.then(function(results){
+			// Recursive if not complete
+			if(searchResults){
+				// Merging results
+        searchResults = {...searchResults, results}
+			}else{
+				searchResults = results
+			}
+			if(results.complete){
+				return (searchResults)
+			}else{
+				return request()
+			}
+		})
+  }
+
   return function (dispatch) {
     dispatch(requestResults(results))
-		// @TODO The url must come from a config file
-    return fetch(`http://localhost:8080/busbud`)
-      .then(
-            response => response.json(),
-            error => console.log('An error occurred.', error)
-      )
-      .then(json => dispatch(receiveResults(results, json)))
+    return request().then(json => dispatch(receiveResults(results, json)))
   }
+
 }
 
 export const RECEIVE_DEPARTURES = 'RECEIVE_DEPARTURES'
