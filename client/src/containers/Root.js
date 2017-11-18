db4a20468d7ba2c0c527425cfb52d80a197bfc4e
Edmon <comlaterra@gmail.com>
2017-11-18 16:33:46 +0100

    Clean up the client

--- client/src/containers/Root.js
+++ client/src/containers/Root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import configureStore from '../configureStore'
+import configureStore from '../stores/configureStore'
 import AsyncApp from './AsyncApp'
 import SearchForm from './SearchForm'
 const store = configureStore()
