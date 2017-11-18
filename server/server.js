10550bd1b1d9dc0d444a49b71ecf13e7ffb45876
Edmon <comlaterra@gmail.com>
2017-11-18 15:18:25 +0100

    Change port

--- server/server.js
+++ server/server.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
