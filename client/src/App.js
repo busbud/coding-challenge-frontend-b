0fe599d0c8b66b1bc6aa11e961a1bd85f1bcf753
Edmon <comlaterra@gmail.com>
2017-11-18 16:21:00 +0100

    Import bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
   render() {
     return (
