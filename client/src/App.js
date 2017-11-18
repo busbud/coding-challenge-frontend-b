21455c4d0bfa90481bf92a8562bae9dc22ee12c1
Edmon <comlaterra@gmail.com>
2017-11-18 16:26:57 +0100

    Move css to styles folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './styles/img/title.png';
-import './App.css';
+import './styles/App.css';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
