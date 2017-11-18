76355bfc8f15283e9563be88d0557900cbf34989
Edmon <comlaterra@gmail.com>
2017-11-18 20:27:56 +0100

    Stylize results List

--- client/src/containers/ListItems.js
+++ client/src/containers/ListItems.js
@@ -8,33 +8,53 @@ class ListItems extends Component {
   }
 
   render(){
+    let self = this
     const listItems = this.state.items.departures.map((departure)=>
       <div className={"result"}>
-        <div className={'row'}>
-          <div className={"col-sm-3"}>
-             {departure.operator_id}
+        <div className={'row top-row'}>
+          <div className={"col-sm-2 logo-operator-wrapper"}>
+             <img className="logo-operator" src={self.state.items.operatorsIdx[departure.operator_id].logo_url} />
           </div>
-          <div className={"col-sm-6"}>
-            <div className={"row"}>
-              <div className={"col-sm-6"}>{departure.departure_time}</div>
-              <div className={"col-sm-6"}>{departure.origin_location_id}</div>
+          <div className={"col-sm-8"}>
+            <div className={"row mid-row"}>
+              <div className={"col-sm-6"}>
+                <div className="middle-text">
+                  {departure.departure_time}
+                </div>
+              </div>
+              <div className={"col-sm-6"}>
+                <div className="middle-text">
+                  {self.state.items.locationsIdx[departure.origin_location_id].name}
+                </div>
+              </div>
             </div>
-            <div className={"row"}>
-              <div className={"col-sm-6"}>{departure.arrival_time}</div>
-              <div className={"col-sm-6"}>{departure.destination_location_id}</div>
+            <div className={"row mid-row"}>
+              <div className={"col-sm-6"}>
+                <div className="middle-text">
+                  {departure.arrival_time}
+                </div>
+              </div>
+              <div className={"col-sm-6"}>
+                <div className="middle-text">
+                  {self.state.items.locationsIdx[departure.destination_location_id].name}
+                </div>
+              </div>
             </div>
           </div>
-          <div className={"col-sm-3"}>
-            {departure.prices.total}
+          <div className={"col-sm-2"}>
+            <button className={"btn btn-warning buy-button"}>
+              <span className={"buy-button-from-text"}>Get it from</span>
+              <span className={"buy-button-from-price"}>{departure.prices.total}</span>
+            </button>
           </div>
         </div>
-        <div className={'row'}>
-          <div className={"col-sm-9"}>
-          </div>
-          <div className={"col-sm-3"}>
-            <input type="submit" value="Select" className={"btn btn-warning"}/>
-          </div>
-        </div>
+        {/*<div className={'row'}>*/}
+          {/*<div className={"col-sm-9"}>*/}
+          {/*</div>*/}
+          {/*<div className={"col-sm-3"}>*/}
+            {/*<input type="submit" value="Select" className={"btn btn-warning"}/>*/}
+          {/*</div>*/}
+        {/*</div>*/}
       </div>
     )
 
