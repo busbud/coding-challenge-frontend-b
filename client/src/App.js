a501c2edaee05ddc3881d3b52eb8bc19fc23ff7b
Edmon <comlaterra@gmail.com>
2017-11-18 16:22:03 +0100

    Implement App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './styles/img/title.png';
 import './App.css';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import Root from './containers/Root'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Root />
       </div>
     );
   }
