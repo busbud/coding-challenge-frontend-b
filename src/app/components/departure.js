f9621c323ab14b6e250eaf8b5d9b7e5c7806e271
Zdobnov <dr.ankh@gmail.com>
2017-12-02 22:24:52 +0100

    findLocation refactored, departure styles fixed

--- src/app/components/departure.js
+++ src/app/components/departure.js
@@ -9,24 +9,29 @@ export default class Departure extends Component {
     currency: PropTypes.string.isRequired
   }
 
-  render() {
+  findLocation(locationName) {
     const {data, departure} = this.props;
-    const locationFrom = data.locations.find(location => location.id === departure['origin_location_id']);
-    const locationTo = data.locations.find(location => location.id === departure['destination_location_id']);
+
+    return data.locations.find(location => location.id === departure[locationName]) || {};
+  }
+
+  render() {
+    const {departure} = this.props;
 
     return (
       <li className="departure">
         <div className="departure__from">
-          <span>{locationFrom.name}</span>
+          <span>{this.findLocation('origin_location_id').name}</span>
           <span>{moment(departure['departure_time']).format('hh:mm a')}</span>
         </div>
         <div className="departure__to">
-          <span>{locationTo.name}</span>
+          <span>{this.findLocation('destination_location_id').name}</span>
           <span>{moment(departure['arrival_time']).format('hh:mm a')}</span>
         </div>
         <div className="departure__price">
-          {(departure.prices.total / 100).toFixed(2)} {this.props.currency}
+          <b>{(departure.prices.total / 100).toFixed(2)} </b> {this.props.currency}
         </div>
+        <a href="#" className="departure__select button">Select</a>
       </li>
     );
   }
