75534db36019eb3ab6bef50c9a217b2233797949
KC\Kwok-Chak <kwokchakw10@gmail.com>
2018-02-04 20:58:50 -0500

    actually didn't need fixing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './src/styles/styles.css';
+import './styles/styles.css';
 import Header from './components/Header.js';
 import Form from './components/Form.js';
 
