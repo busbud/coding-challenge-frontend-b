cb87b994de13572bd759a63ff76feb4035375ab5
Steven Bengtson <steven.bengtson@me.com>
2018-03-24 07:01:16 -0700

    Add security and styling

--- app.js
+++ app.js
@@ -1,48 +1,67 @@
 var createError = require('http-errors');
 var express = require('express');
-var helmet = require('helmet')
+var helmet = require('helmet');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var csurf = require('csurf');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
+// Define routes
 var indexRouter = require('./routes/index');
 
+// Setup express application
 var app = express();
 
 // Secure application a bit
-app.use(helmet())
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Setup middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
+  indentedSyntax: true,
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Non CSRF Protected routes here
 app.use('/', indexRouter);
+
+// Setup CSRF
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(csurf({ cookie: true }));
+
+// CSRF Protected routes here
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// Catch any CSRF errors
+app.use(function (err, req, res, next) {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  res.status(403);
+  res.render('csrf_error');
+});
+
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
