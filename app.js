6752a4bde9caf5570ec9825e4f6b1311246e984c
Steven Bengtson <steven.bengtson@me.com>
2018-03-24 06:06:28 -0700

    Add helmet for security

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var helmet = require('helmet')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +9,9 @@ var sassMiddleware = require('node-sass-middleware');
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+// Secure application a bit
+app.use(helmet())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
