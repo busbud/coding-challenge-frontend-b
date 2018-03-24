76c5bca61df4f5edd67f73da4eb03c933dc2a1ca
Steven Bengtson <steven.bengtson@me.com>
2018-03-24 12:07:17 -0700

    Work on polling

--- routes/search.js
+++ routes/search.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var busbud = require("../lib/busbud");
 
 router.post("/", function(req, res, next) {
-  let sourceCityGeoHash = 'dr5reg';
-  let destCityGeoHash = 'f25dvk';
-  let travelDate = '2018-08-02';
+  let sourceCityGeoHash = req.param('originLocationGeoHash', 'dr5reg');
+  let destCityGeoHash = req.param('destinationLocationGeoHash', 'f25dvk');
+  let travelDate = req.param('departureDate', '2018-08-02');
   let options = {
     adult: 1
   };
@@ -13,6 +13,8 @@ router.post("/", function(req, res, next) {
   busbud.search(sourceCityGeoHash, destCityGeoHash, travelDate, options).then(function(search_data) {
     let parsedData = busbud.parseDepartures(search_data);
     res.render("index", { data: parsedData, csrfToken: req.csrfToken() });
+  }).catch(function(error) {
+    res.render("index", { csrfToken: req.csrfToken() });
   });
 });
 
