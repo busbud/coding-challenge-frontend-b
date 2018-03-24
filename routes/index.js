9fea87e217683dea5fe8ed779b0b21e6c299a9f8
Steven Bengtson <steven.bengtson@me.com>
2018-03-24 08:00:14 -0700

    Start of BusBud library

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var busbud = require("../lib/busbud");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  let search_data = busbud.search("dr5reg", "f25dvk", "2018-08-02");
+  res.render("index", { data: search_data });
 });
 
 module.exports = router;
