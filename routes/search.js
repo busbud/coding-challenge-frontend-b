2fbaf9691806e47b85ce0cb5eff547b3400634db
Steven Bengtson <steven.bengtson@me.com>
2018-03-25 10:20:51 -0700

    Search form cleanup

--- routes/search.js
+++ routes/search.js
@@ -2,15 +2,18 @@ var express = require("express");
 var router = express.Router();
 var busbud = require("../lib/busbud");
 
-router.post("/", function(req, res, next) {
-  let sourceCityGeoHash = req.param('originLocationGeoHash', 'dr5reg');
-  let destCityGeoHash = req.param('destinationLocationGeoHash', 'f25dvk');
-  let travelDate = req.param('departureDate', '2018-08-02');
-  let options = {
-    adult: 1
-  };
+router.post("/", function(req, res) {
+  let params = req.body;
+  let sourceCityGeoHash = params.originLocationGeoHash;
+  let destCityGeoHash = params.destinationLocationGeoHash;
+  let travelDate = params.departureDate;
+  
+  delete params.originLocationGeoHash;
+  delete params.destinationLocationGeoHash;
+  delete params.departureDate;
+  delete params._csrf;
 
-  busbud.search(sourceCityGeoHash, destCityGeoHash, travelDate, options).then(function(search_data) {
+  busbud.search(sourceCityGeoHash, destCityGeoHash, travelDate, params).then(function(search_data) {
     let parsedData = busbud.parseDepartures(search_data);
     res.render("index", { data: parsedData, csrfToken: req.csrfToken() });
   }).catch(function(error) {
