c1eace6fbe1d79ca5b55b76efaf8de770e7cf670
Alexander Chalk <alexchalk17@gmail.com>
2018-04-27 11:23:26 -0400

    Correct departure date

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -9,7 +9,7 @@ const AppHeader = () => {
         <h1 className="App-title">Get to Osheaga!</h1>
       </header>
       <p className="App-subheader">
-      Find tickets from New York to Montréal on August 3rd, 2018:
+      Find tickets from New York to Montréal on August 2nd, 2018:
       </p>
     </div>
   );
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const busbudApiToken = process.env.REACT_APP_X_TOKEN_API;
-export const apiURL = 'https://napi.busbud.com/x-departures/dr5reg/f25dvk/2018-08-03';
+export const apiURL = 'https://napi.busbud.com/x-departures/dr5reg/f25dvk/2018-08-02';
