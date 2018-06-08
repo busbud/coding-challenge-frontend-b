aa622a1690ed4ce1515690f25198e39499a73693
Rémi THOMAS <remi.thomas@dash2dots.com>
2018-06-08 01:09:04 -0400

    Handle error + refactor

--- src/components/DeparturesContainer.jsx
+++ src/components/DeparturesContainer.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Loading from './Loading';
 import DepartureList from './DepartureList';
+import ErrorMessage from './ErrorMessage';
 
 import { searchDepartures } from '../utils/Api';
 import { mapCitiesToDepartures } from '../utils/Departures';
@@ -14,30 +15,61 @@ class DeparturesContainer extends Component {
     this.state = {
       departures: [],
       isLoading: true,
+      hasFail: false,
     };
+
+    this.handleSearchDepartures = this.handleSearchDepartures.bind(this);
+    this.handleSearchDeparturesFailure = this.handleSearchDeparturesFailure.bind(this);
+    this.handleSearchDeparturesSuccess = this.handleSearchDeparturesSuccess.bind(this);
+    this.handleRestartSearch = this.handleRestartSearch.bind(this);
   }
 
   componentDidMount() {
+    this.handleSearchDepartures();
+  }
+
+  handleSearchDepartures() {
     const {
       origin, destination, outboundDate, language,
     } = this.props;
 
     searchDepartures({
       origin, destination, outboundDate, language,
-    }).then(({ locations, departures }) => {
-      const completeDepartures = mapCitiesToDepartures(locations, departures);
-      this.setState({
-        departures: completeDepartures,
-        isLoading: false,
-      });
+    })
+      .then(({ locations, departures }) => this.handleSearchDeparturesSuccess(locations, departures))
+      .catch(() => this.handleSearchDeparturesFailure());
+  }
+
+  handleSearchDeparturesFailure() {
+    this.setState({
+      hasFail: true,
+      isLoading: false,
     });
   }
 
+  handleSearchDeparturesSuccess(locations, departures) {
+    const completeDepartures = mapCitiesToDepartures(locations, departures);
+    this.setState({
+      departures: completeDepartures,
+      isLoading: false,
+    });
+  }
+
+  handleRestartSearch() {
+    this.setState({
+      isLoading: true,
+    });
+
+    this.handleSearchDepartures();
+  }
+
   render() {
-    const { isLoading, departures } = this.state;
+    const { isLoading, departures, hasFail } = this.state;
 
     return (
       <div className="DeparturesContainer">
+        {hasFail && <ErrorMessage onRestartSearch={() => this.handleRestartSearch()} />}
+
         {isLoading && <Loading />}
 
         <DepartureList departures={departures} />
---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { translate } from 'react-i18next';
+
+import './styles/ErrorMessage.scss';
+
+function ErrorMessage(props) {
+  const { onRestartSearch, t } = props;
+
+  return (
+    <div className="ErrorMessage">
+      <div className="ErrorMessage__message">{t('error')}</div>
+      <button onClick={() => onRestartSearch()}>
+        {t('restart')}
+      </button>
+    </div>
+  );
+}
+
+ErrorMessage.propTypes = {
+  onRestartSearch: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
+};
+
+export default translate('translations')(ErrorMessage);
