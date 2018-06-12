87b9427136e825569a2f9ff443bd3ac333229d05
Alexis PARIS <alexis.paris@blackdog-project.org>
2018-06-11 20:25:32 -0400

    initialize project with vue cli add dependencies : bootstrap, vue-resource enable support for sass

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <b-alert show>Default Alert</b-alert>
+    <!--img src="./assets/logo.png"-->
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
