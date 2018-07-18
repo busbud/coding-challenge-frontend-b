4507d8bc2553964ed62a14e1f2601789ae7b6269
Matheus Ramos <matheusedsonramos@gmail.com>
2018-07-18 12:31:10 -0300

    Sets the default date to match the challenge description

--- src/components/SearchHeader.js
+++ src/components/SearchHeader.js
@@ -82,7 +82,7 @@ const SearchHeader = (props) => {
               </div>
               <div className="field">
                 <p className="control has-icons-left">
-                  <input className="input" name="outbound_date" type="date" defaultValue="2018-07-18" />
+                  <input className="input" name="outbound_date" type="date" defaultValue="2018-08-02" />
                   <span className="icon is-small is-left"><i className="fa fa-calendar-o"></i></span>
                 </p>
               </div>
