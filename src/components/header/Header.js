886a313a71e2e0fe0001a927b065cc6a4273575b
Antech G <40809114+antechg@users.noreply.github.com>
2018-07-20 00:43:52 -0400

    Fix header. Remove unless dependence

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -29,7 +29,7 @@ class Header extends Component {
 
   render() {
     return (
-      <AppBar position="static" color="default">
+      <AppBar position="fixed" color="default">
         <Toolbar>
           <IconButton color="inherit" aria-label="Menu" onClick={this.openMenu}>
             <MenuIcon />
