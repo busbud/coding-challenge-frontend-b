fd8b54d1ba30b76efca69a349fd6e85fdd7366a2
Gaëlle Altefrohne <galtefrohne@connectiv-it.com>
2018-09-11 22:54:24 +0200

    Test with HTML and bootstrap.

--- busbudApp/routes/index.js
+++ busbudApp/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.resolve("views/index.html"));
 });
 
 module.exports = router;
