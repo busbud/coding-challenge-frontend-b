cfb7a12bdcd226c35807a3961bfa8496e07ad610
Gaëlle Altefrohne <galtefrohne@connectiv-it.com>
2018-09-13 01:36:49 +0200

    jade template skeleton and basic navigation.

--- busbudApp/app.js
+++ busbudApp/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var contactRouter = require('./routes/contact');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
