61adb7e287e20e3c39d9e0a3109f9af7062b570f
AkshayBorana <akshayborana1@gmail.com>
2018-09-18 15:17:32 -0400

    changes made in the service.ts file.

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Http } from '@angular/http';
 // import { map } from 'rxjs/operators';
 import 'rxjs/add/operator/map';
@@ -8,10 +8,15 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class HomeService {
 
+    // let httpOptions = {
+    //     headers: new HttpHeaders({'Accept': 'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/', 'X-Busbud-Token':'PARTNER_AHm3M6clSAOoyJg4KyCg7w' })
+    // }
+
     constructor(private http: HttpClient) {}
 
     getData(today:any):Observable<any> {
-        return this.http.get('https://napi.busbud.com/x-departures/dr5reg/f25dvk/'+today).map(res => {
+        return this.http.get('https://napi.busbud.com/prices.total/dr5reg/f25dvk/'+today,{headers: new HttpHeaders({'Accept': 'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/', 'X-Busbud-Token':'PARTNER_AHm3M6clSAOoyJg4KyCg7w' })})
+        .map(res => {
             console.log(res);
             return JSON.parse(res['_body']);
     })
