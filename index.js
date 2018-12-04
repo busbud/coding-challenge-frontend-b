e0b51a0c2c405548a7896694d109cab415f7fea8
Matthieu Moreau <dev@matthieumoreau.fr>
2018-12-04 01:14:52 +0100

    Init app with Create React App

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const path = require("path");
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "temp")));
+app.use(express.static(path.join(__dirname, "app/build")));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/temp/index.html"));
+  res.sendFile(path.join(__dirname + "/app/build/index.html"));
 });
 
 const port = process.env.PORT || 1337;
