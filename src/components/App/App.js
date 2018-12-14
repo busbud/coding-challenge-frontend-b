0caf894503cfa4171b9e79383c21f3f56c8a4812
BertrandD <bernaz@pm.me>
2018-12-14 15:43:08 +0100

    update file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+// @flow
+import React from 'react';
+import './App.css';
+
+export type Props = {||};
+
+export const App = (): Props => (
+  <div>
+    {' '}
+    <p>
+      {' '}
+      {'HEllo'}
+      {' '}
+    </p>
+    {' '}
+  </div>
+);
