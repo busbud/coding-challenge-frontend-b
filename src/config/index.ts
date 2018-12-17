f6cd352ec1af027dd57ade3d05a2837a97df6dbd
Genevieve Perron-Migneron <info@gen-migneron.com>
2018-12-17 16:32:10 -0500

    config file

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+export const config = {
+  baseURL: 'https://napi.busbud.com/x-departures/',
+  headers: {
+    Accept: 'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
+    'X-Busbud-Token': 'PARTNER_AHm3M6clSAOoyJg4KyCg7w'
+  }
+}
