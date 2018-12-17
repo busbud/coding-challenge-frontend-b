a67910fbb8b2aff067fed1e7ccfefe5886035f77
Genevieve Perron-Migneron <info@gen-migneron.com>
2018-12-17 01:54:08 -0500

    Add Normalize and bootstrap grid. Build sass setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { render } from 'react-dom'
 import * as React from 'react'
+import './css/application.sass'
 
 render(
   <h1>My app</h1>,
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 let config = {
-  entry: ['./src/app.tsx'],
+  entry: ['./src/app.tsx', './src/css/application.sass'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js',
@@ -26,7 +26,15 @@ let config = {
         test: /\.tsx?/,
         loader: 'ts-loader',
         exclude: [/node_modules/]
-      }
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
     ]
   }
 }
