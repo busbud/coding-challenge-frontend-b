d67a7b51ac0e2c330b9fdc4ac10319891538bb9c
Genevieve Perron-Migneron <info@gen-migneron.com>
2018-12-18 02:15:13 -0500

    ignore dist directory, add heroku postbuild config

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-const port = 3000
+const port = 8080
 
-app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, "/dist/index.html"))
