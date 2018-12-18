d95faf6b605c580cb4ba74b5302e6f06a6efde92
Genevieve Perron-Migneron <info@gen-migneron.com>
2018-12-18 02:19:59 -0500

    fix directory and specify node version

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 8080
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/index.html"))
+  res.sendFile(path.join(__dirname, "dist/index.html"))
 });
 
 app.listen(process.env.PORT || port)
