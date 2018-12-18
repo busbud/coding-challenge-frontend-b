fa546ec21f283e1fa3fde8faa19dfb9c888b2b79
BertrandD <bernaz@pm.me>
2018-12-18 02:46:56 +0100

    lol

---
+++ build/precache-manifest.8453e1204a2cb668f8f01585e0b6f281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cabda41886c52163be0",
+    "url": "/static/css/main.d85de94a.chunk.css"
+  },
+  {
+    "revision": "7cabda41886c52163be0",
+    "url": "/static/js/main.7cabda41.chunk.js"
+  },
+  {
+    "revision": "7bc1d4703a52e98f954c",
+    "url": "/static/js/1.7bc1d470.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6cd366ab910ff497a7ca20bf6b7025c9",
+    "url": "/index.html"
+  }
+];
