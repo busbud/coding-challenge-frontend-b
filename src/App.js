19fe5224ce746dd740f33ce8f2f67dce3ddfd8db
mikolaj <mikolaj.burawski@gmail.com>
2019-02-25 05:50:17 +0000

    add react testing library

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Welcome to React
           </a>
         </header>
       </div>
