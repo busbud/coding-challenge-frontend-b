ca1193026cf7f9e0c158089eb9e61df4cc9b4688
vanessa <vcianciarelli@gmail.com>
2019-07-19 23:08:52 -0400

    Prettify the header

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -4,7 +4,7 @@ export default class HeaderContainer extends React.Component {
   render() {
     return (
       <div className="top-bar-container">
-        <i className="fa fa-angle-left fa-lg" />
+        <i className="fa fa-angle-left" />
         <div className="language">EN</div>
       </div>
     );
