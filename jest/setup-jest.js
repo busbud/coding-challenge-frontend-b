8fa236ad4c19740df78ffb768ddbfac9e11c2a2f
vanessa <vcianciarelli@gmail.com>
2019-07-22 19:51:41 -0400

    Jest to test AttributeSelection component

--- jest/setup-jest.js
+++ jest/setup-jest.js
@@ -1,4 +1,10 @@
+// mocking lodash
 import lodash from 'lodash';
+
 global._ = lodash;
 
 jest.mock('lodash/map');
+
+// to avoid triggering navigation in tests
+delete window.location;
+window.location = { reload: jest.fn() };
--- src/components/AttributeSelection.test.js
+++ src/components/AttributeSelection.test.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import AttributeSelection from './AttributeSelection';
 
 const props = {
   getCurrentValue: () => 'CAD',
+  changeValue: () => {},
 };
 
-describe('Attribute selection component', () => {
-  test('renders', () => {
-    const wrapper = shallow(<AttributeSelection {...props} />);
+describe('Attribute selection component', function() {
+  beforeEach(() => {
+    this.wrapper = shallow(<AttributeSelection {...props} />);
+  });
 
-    expect(wrapper.exists()).toBe(true);
+  test('renders', () => {
+    expect(this.wrapper.exists()).toBe(true);
+  });
+
+  test("user's currency selection is echoed and page is refreshed", () => {
+    this.wrapper.find('select').simulate('change', {
+      target: { value: 'USD' },
+    });
+
+    window.location.reload();
+
+    expect(this.wrapper.find('select').props().value).toEqual('USD');
+    expect(window.location.reload).toHaveBeenCalled();
   });
 });
