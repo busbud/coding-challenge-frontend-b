20c737564ac877060dfdc1e5742688d95204c201
vanessa <vcianciarelli@gmail.com>
2019-07-23 00:22:00 -0400

    Test component fetching departures

---
+++ __tests__/DeparturesContainer.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import DeparturesContainer from '../src/containers/DeparturesContainer';
+
+jest.mock('../src/services/fetch-departures-service', () => ({
+  fetchDepartures: () => ({
+    complete: true,
+    departures: [
+      {
+        busbud_departure_id: 1,
+        departureTime: '10:00',
+        arrivalTime: '14:00',
+        price: '6800',
+      },
+      {
+        busbud_departure_id: 2,
+        departureTime: '11:00',
+        arrivalTime: '13:00',
+        price: '6200',
+      },
+    ],
+    locations: [
+      {
+        id: 1,
+        name: 'Station A',
+      },
+      {
+        id: 1,
+        name: 'Station B',
+      },
+    ],
+  }),
+}));
+
+jest.mock('../src/services/attribute-service', () => ({
+  getCurrentLanguage: () => 'EN',
+}));
+
+describe.only('Departures container', function() {
+  beforeEach(() => {
+    this.wrapper = shallow(<DeparturesContainer />);
+  });
+
+  test('renders', () => {
+    expect(this.wrapper.exists()).toBe(true);
+  });
+
+  test('should update departures state', () => {
+    expect(this.wrapper.state().data.departures).toHaveLength(2);
+    expect(this.wrapper.state().data.locations).toHaveLength(2);
+    expect(this.wrapper.state().data.departures[1].price).toEqual('6200');
+  });
+});
--- src/utils/format-departures-data-helper.js
+++ src/utils/format-departures-data-helper.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export function filterOutDuplicateData(stateData, data) {
   return _.filter(data, el => {
     if (!_.some(stateData, stateEL => el.id === stateEL.id)) {
