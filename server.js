674cc019ae3c6fa4af5cbbb710290f0ac0b449aa
vanessa <vcianciarelli@gmail.com>
2019-07-23 22:23:06 -0400

    Fix server serving static routes

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/dist'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist/index.html'));
 });
 
 app.listen(port);
