8d982760580886d8fcdec3a6e16446915685ead7
vanessa <vcianciarelli@gmail.com>
2019-07-23 23:43:09 -0400

    Refactor token is stored in server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
+app.get('/cred', (req, res) => {
+  res.json({ cred: process.env.X_BUSBUD_TOKEN || '' });
+});
+
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './dist/index.html'));
 });
--- src/services/fetch-departures-service.js
+++ src/services/fetch-departures-service.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 import { getCurrentLanguage } from '../services/attribute-service';
 
-const headers = {
-  Accept:
-    'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
-  'X-Busbud-Token': `${process.env.X_BUSBUD_TOKEN}`,
-};
+async function getHeader() {
+  let token = process.env.X_BUSBUD_TOKEN;
+  if (!token) {
+    const response = await axios.get('/cred');
+    token = response.data.cred;
+  }
+  return {
+    Accept:
+      'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
+    'X-Busbud-Token': `${token}`,
+  };
+}
 
 const language = getCurrentLanguage();
 const { currency } = window.localStorage;
@@ -18,7 +25,7 @@ export async function fetchDepartures(index, searchDay) {
   const response = await axios({
     method: 'get',
     url: generateUrl(index, searchDay),
-    headers,
+    headers: await getHeader(),
   });
   return response.data;
 }
