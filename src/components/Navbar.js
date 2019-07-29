f3581bec230f8d5d43298ca858071d18f188836b
Paulo Moreno <paulomorenoo@gmail.com>
2019-07-29 19:37:06 -0300

    feat: add dropdown to change language on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { Translate } from 'react-redux-i18n';
+import Container from 'react-bootstrap/Container';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Translate, setLocale } from 'react-redux-i18n';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { createStructuredSelector } from 'reselect';
 
-const CustomNavbar = () => (
+const locales = ['en', 'pt', 'fr'];
+
+const CustomNavbar = ({ locale, setLocaleConnect }) => (
   <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
-    <Navbar.Brand>
-      <Translate value="application.title" />
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end text-white">
-      <Nav />
-    </Navbar.Collapse>
+    <Container>
+      <Navbar.Brand>
+        <Translate value="application.title" />
+      </Navbar.Brand>
+      <Navbar className="justify-content-end">
+        <NavDropdown title={locale} id="basic-nav-dropdown">
+          {locales.map(localeCode => (
+            <NavDropdown.Item
+              key={`navbarLocale_${localeCode}`}
+              as="button"
+              onClick={() => {
+                setLocaleConnect(localeCode);
+              }}
+            >
+              <Translate value={`application.languages.${localeCode}`} />
+            </NavDropdown.Item>
+          ))}
+        </NavDropdown>
+      </Navbar>
+    </Container>
   </Navbar>
 );
-export default CustomNavbar;
+
+CustomNavbar.propTypes = {
+  locale: PropTypes.string.isRequired,
+  setLocaleConnect: PropTypes.func.isRequired,
+};
+
+const localeSelector = state => state.i18n.locale;
+
+const mapStateToPropsSelector = createStructuredSelector({
+  locale: localeSelector,
+});
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    setLocaleConnect: setLocale,
+  }, dispatch);
+}
+
+export default connect(mapStateToPropsSelector, mapDispatchToProps)(CustomNavbar);
--- src/utils/en.js
+++ src/utils/en.js
@@ -1,5 +1,10 @@
 export default {
   application: {
     title: 'Oshaega Planner',
+    languages: {
+      en: 'English',
+      pt: 'Português',
+      fr: 'Français',
+    },
   },
 };
