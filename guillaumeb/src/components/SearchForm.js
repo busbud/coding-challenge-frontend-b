ef1b5dbfed3432bfd1d8ae04c5be6f531470ee8a
Guillaume Bourrousse <gbourrousse@gmail.com>
2019-08-19 15:24:09 -0400

    Build departure Array with correct Data
    Map to a departures array with correct info like times, prices, and locations

--- guillaumeb/src/components/SearchForm.js
+++ guillaumeb/src/components/SearchForm.js
@@ -3,31 +3,55 @@ import React, { Component } from 'react';
 class SearchForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isLoading: false,
+      departures: [],
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   searchResults = () => {
-    const apiUrl =
-      'https://napi.busbud.com/x-departures/f25dvk/dr5reg/2020-08-02';
+    this.setState({ isLoading: true }, () => {
+      const apiUrl =
+        'https://napi.busbud.com/x-departures/f25dvk/dr5reg/2020-08-02';
 
-    const requestOptions = {
-      method: 'GET',
-      headers: {
-        Accept:
-          'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
-        'X-Busbud-Token': 'PARTNER_AHm3M6clSAOoyJg4KyCg7w',
-      },
-    };
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          Accept:
+            'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
+          'X-Busbud-Token': 'PARTNER_AHm3M6clSAOoyJg4KyCg7w',
+        },
+      };
 
-    fetch(apiUrl, requestOptions)
-      .then(results => results.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+      fetch(apiUrl, requestOptions)
+        .then(results => results.json())
+        .then(data => {
+          const locations = data.locations.map(location => ({
+            id: location.id || '',
+            name: location.name || '',
+          }));
+
+          const departures = data.departures.map(departure => ({
+            departure_time: departure.departure_time || '',
+            arrival_time: departure.arrival_time || '',
+            price: departure.prices.total,
+            location: locations.find(
+              location => location.id === departure.destination_location_id
+            ),
+          }));
+
+          this.setState({
+            isLoading: false,
+            departures: [...departures],
+          });
+
+          console.log(departures);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
   };
 
   handleSubmit(event) {
@@ -36,9 +60,11 @@ class SearchForm extends Component {
   }
 
   render() {
+    const { isLoading } = this.state;
+
     return (
       <form onSubmit={this.handleSubmit}>
-        <p>Search Form</p>
+        <p>Search Form {isLoading && <span>Loading...</span>}</p>
         <input type="submit" value="Submit" />
       </form>
     );
