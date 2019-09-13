5895438afe7e62a1b6c19e66a04d96dc0ec92b64
Valentijn Nieman <valentijnnieman@gmail.com>
2019-09-13 15:31:07 -0400

    Added disabled form

--- src/components/DeparturesForm.tsx
+++ src/components/DeparturesForm.tsx
@@ -10,11 +10,30 @@ interface DeparturesFormProps {
 
 const DeparturesForm: React.FunctionComponent<DeparturesFormProps> = props => {
   return (
-    <div className="bus-list-card">
-      <h1 className="bus-list-title">{props.title}</h1>
+    <div className="bus-form-card">
+      <h1 className="bus-form-title">{props.title}</h1>
+      <div className="bus-form">
+        <div>
+          <input
+            className="bus-form__input"
+            value="New York"
+            disabled={true}
+          ></input>
+          <input
+            className="bus-form__input"
+            value="Montreal"
+            disabled={true}
+          ></input>
+        </div>
+        <input
+          className="bus-form__input bus-form__input--long"
+          value="August 2nd, 2020"
+          disabled={true}
+        ></input>
+      </div>
       <Button
         onClick={props.searchDepartures}
-        className="bus-list-find-button"
+        className="bus-form__submit"
         type="primary"
         size="big"
       >
