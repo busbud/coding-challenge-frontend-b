2900b1e1ce4f3e1dad1c16d18d5539570a49ebe2
sunilpuranik <puranikedsunil@gmail.com>
2019-10-14 01:28:10 -0400

    Api key issue

--- src/epics/departuresEpic.js
+++ src/epics/departuresEpic.js
@@ -19,7 +19,7 @@ export const fetchEpic = action$ =>
         headers: {
           Accept:
             'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
-          'X-Busbud-Token': process.env.REACT_APP_API_KEY,
+          'X-Busbud-Token': 'PARTNER_AHm3M6clSAOoyJg4KyCg7w',
         },
       }).pipe(map(fetchDeparturesSuccess)),
     ),
