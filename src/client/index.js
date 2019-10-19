4aeb10e3dc958228d1a1ccdd36c6f74dde77eed3
Josh Kim <joshua940308@gmail.com>
2019-10-19 01:51:33 -0400

    from scss to css

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/main.scss';
+import './styles/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
       },
     ],
   },
