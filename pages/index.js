766623860e90c1660a6c6a30394ba24c2f32c88d
Swapnil Patil <swapnil00patil@gmail.com>
2019-10-31 20:50:31 +0800

    added styled-components support

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 import Head from 'next/head'
 import Nav from '../components/nav'
 
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`
+
 const Home = () => (
   <div>
+    <Title>This is title</Title>
     <Head>
       <div className="title">Test</div>
       <link rel='icon' href='/favicon.ico' />
