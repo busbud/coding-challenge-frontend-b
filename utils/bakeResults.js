91d46f75317c91715ad950b3bd10196bad53cd93
Swapnil Patil <swapnil00patil@gmail.com>
2019-11-02 21:50:56 +0800

    moved baseResults util outside as next expect page

--- utils/bakeResults.js
+++ utils/bakeResults.js
@@ -1,31 +1,34 @@
 export const bakeDepartureResults = (results, response) => {
-  if(!results.departures) {
+  if (!results.departures) {
     results = {
       departures: []
-    }
+    };
   }
   // csorted departure dates
-  if(response && response.departures && response.departures.length > 0) {
-    results.departures = sortByDate([...results.departures, ...response.departures], 'departure_time');
+  if (response && response.departures && response.departures.length > 0) {
+    results.departures = sortByDate(
+      [...results.departures, ...response.departures],
+      "departure_time"
+    );
   }
   // converting to object so we dont have to loop it later
-  if(response && response.locations && response.locations.length > 0) {
-    results.locations = convertArrayToObj(response.locations, 'id');
+  if (response && response.locations && response.locations.length > 0) {
+    results.locations = convertArrayToObj(response.locations, "id");
   }
   // converting to object so we dont have to loop it later
-  if(response && response.operators && response.operators.length > 0) {
-    results.operators = convertArrayToObj(response.operators, 'id');
+  if (response && response.operators && response.operators.length > 0) {
+    results.operators = convertArrayToObj(response.operators, "id");
   }
   return results;
-}
+};
 
 const convertArrayToObj = (arr, key) => {
   return arr.reduce((accumulator, current) => {
-      accumulator[current[key]] = current;
-     return accumulator;
+    accumulator[current[key]] = current;
+    return accumulator;
   }, {});
-}
- 
+};
+
 const sortByDate = (results, key) => {
   return results.sort((a, b) => {
     const aDate = new Date(a[key]);
@@ -37,5 +40,5 @@ const sortByDate = (results, key) => {
       return 1;
     }
     return 0;
-  })
-}
+  });
+};
