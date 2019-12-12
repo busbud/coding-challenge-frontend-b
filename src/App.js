2051ddcdfe70889f3b8cd18c3519702dec8f1ec3
Floriane <floriane@tuta.io>
2019-12-11 19:01:38 -0500

    Init app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> Hello, World! </h1>
+    </div>
+  );
+};
+
+export default hot(module)(App);
