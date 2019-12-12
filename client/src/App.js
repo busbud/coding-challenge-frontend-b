ba7a988519f6aa5f01f1f21b71bdb22a961c5ba1
Floriane <floriane@tuta.io>
2019-12-12 14:33:32 -0500

    Removes Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { hot } from "react-hot-loader";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
 import Home from "./components/Home";
 import "./App.scss";
 
-const client = new ApolloClient({
-  uri: "https://localhost:4000"
-});
-
 const App = () => {
   return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <Home />
-      </div>
-    </ApolloProvider>
+    <div className="App">
+      <Home />
+    </div>
   );
 };
 
