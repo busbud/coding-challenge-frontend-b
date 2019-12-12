e4a4c0c286c10ad412fb9414244056a8a6e27718
Floriane <floriane@tuta.io>
2019-12-12 14:25:40 -0500

    Make it simpler and removes Apollo & graphQL

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,65 +1,90 @@
 import React, { useState } from "react";
-import gql from "graphql-tag";
-import { useLazyQuery } from "@apollo/react-hooks";
 import "./Home.scss";
 
-const GET_DEPARTURES = gql`
-  query Departures($from: String!, $to: String!, $when: String!) {
-    departures(when: $when, to: $to, from: $from) {
-      departure_time
-      arrival_time
-      location
-      price
-    }
-  }
-`;
+const formatDeparture = ({
+  arrival_time,
+  available_seats,
+  complete,
+  departure_time,
+  duration,
+  id,
+  prices,
+  ...departure
+}) => {
+  return {
+    arrival_time,
+    available_seats,
+    seatClass: departure.class,
+    complete,
+    departure_time,
+    duration,
+    id,
+    prices
+  };
+};
 
 const Home = () => {
   const [departures, setDepartures] = useState(null);
-  const [getDepartures, { loading, data, error }] = useLazyQuery(
-    GET_DEPARTURES
-  );
+  const [error, setError] = useState(null);
 
-  if (loading) return <p>"Loading..."</p>;
-
-  if (data && data.departures) {
-    setDepartures(data.departures);
-  }
+  const getDepartures = e => {
+    e.preventDefault();
+    fetch("https://napi.busbud.com/x-departures/dr5reg/f25dvk/2020-08-02", {
+      headers: {
+        Accept:
+          "application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/",
+        "X-Busbud-Token": "PARTNER_AHm3M6clSAOoyJg4KyCg7w"
+      }
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          result.departures &&
+            setDepartures(result.departures.map(formatDeparture));
+        },
+        error => {
+          setError(error);
+        }
+      );
+  };
 
   return (
     <div className="home">
       <h1>Find your way to Osheaga</h1>
-      <form
-        className="home__search"
-        onSubmit={getDepartures({
-          variables: {
-            from: "dr5reg",
-            to: "f25dvk",
-            when: new Date("08/02/2020").toISOString().split("T", 1)[0]
-          }
-        })}
-      >
+      <form className="home__search" onSubmit={getDepartures}>
         {error && `Error! ${error.message}`}
         <div className="home__search__fields">
           <div className="home__search__fields__input">
             <label htmlFor="from">From</label>
-            <input id="from" type="text" name="from" value="New-York" />
+            <input
+              readOnly
+              id="from"
+              type="text"
+              name="from"
+              value="New-York"
+            />
           </div>
           <div className="home__search__fields__input">
             <label htmlFor="to">To</label>
-            <input id="to" type="text" name="to" value="Montreal" />
+            <input readOnly id="to" type="text" name="to" value="Montreal" />
           </div>
           <div className="home__search__fields__input">
             <label htmlFor="date">When</label>
-            <input id="when" type="text" name="when" value="08/02/2020" />
+            <input
+              readOnly
+              id="when"
+              type="text"
+              name="when"
+              value="08/02/2020"
+            />
           </div>
         </div>
         <input className="home__search__submit" type="submit" value="Search" />
       </form>
       {departures && (
         <div>
-          {departures.map(departure => (
-            <div>{departure.departure_time}</div>
+          {departures.map(({ id, ...departure }) => (
+            <div key={id}>{departure.departure_time}</div>
           ))}
         </div>
       )}
