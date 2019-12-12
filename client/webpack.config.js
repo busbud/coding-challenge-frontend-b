1be2142775ead0a16d7478a316a02baf929ddbc5
Floriane <floriane@tuta.io>
2019-12-12 11:24:04 -0500

    Installs Sass

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
