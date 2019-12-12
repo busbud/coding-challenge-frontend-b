ccbbfa437ce558f1599f7f9f49344fe0b4ac4047
Floriane <floriane@tuta.io>
2019-12-12 10:47:26 -0500

    Sets up Apollo client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { hot } from "react-hot-loader";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 import "./App.css";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
 
 const App = () => {
   return (
-    <div className="App">
-      <h1> Hello, World! </h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1> Hello, World! </h1>
+      </div>
+    </ApolloProvider>
   );
 };
 
