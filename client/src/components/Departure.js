5db8a18a13c07626de9f27d6642952ede89dc530
Floriane <floriane@tuta.io>
2019-12-12 18:40:35 -0500

    Implements localization

--- client/src/components/Departure.js
+++ client/src/components/Departure.js
@@ -1,5 +1,27 @@
 import React from "react";
+import LocalizedStrings from "react-localization";
 import "./Departure.scss";
+
+let strings = new LocalizedStrings({
+  en: {
+    from: "From",
+    to: "To",
+    when: "When",
+    book: "Book",
+    duration: "Duration: ",
+    complete: "Complete",
+    seats: "Available seats:"
+  },
+  fr: {
+    from: "Depuis",
+    to: "Vers",
+    when: "Date",
+    book: "Réserver",
+    duration: "Durée : ",
+    complete: "Complet",
+    seats: "Sièges disponibles :"
+  }
+});
 
 const Departure = ({
   arrival_time,
@@ -10,36 +32,42 @@ const Departure = ({
   prices,
   seatClass,
   from,
-  to
+  to,
+  lang
 }) => {
+  strings.setLanguage(lang);
   return (
     <div className={`departure ${complete ? "departure--complete" : ""}`}>
       <div className="departure__content">
-        <p className="departure__content__label">From</p>
+        <p className="departure__content__label">{strings.from}</p>
         <div className="departure__content__location">
           <p>{from}</p>
-          <p>{new Date(departure_time).toLocaleString()}</p>
+          <p>{new Date(departure_time).toLocaleString(lang)}</p>
         </div>
         <p className="departure__content__arrow">&rarr;</p>
-        <p className="departure__content__label">To</p>
+        <p className="departure__content__label">{strings.to}</p>
         <div className="departure__content__location">
           <p>{to}</p>
-          <p>{new Date(arrival_time).toLocaleString()}</p>
+          <p>{new Date(arrival_time).toLocaleString(lang)}</p>
         </div>
         <div className="departure__content__duration">
-          <span>Duration: </span>
+          <span>{strings.duration}</span>
           {duration}
         </div>
       </div>
       <div className="departure__reservation">
         <div className="departure__reservation__availability">
           <div>{seatClass}</div>
-          <p>{complete ? "Complete" : `Available seats: ${available_seats}`}</p>
+          <p>
+            {complete
+              ? strings.complete
+              : `${strings.seats} ${available_seats}`}
+          </p>
         </div>
         <div className="departure__reservation__price">
           {prices.total} {prices.currency}
         </div>
-        <button>Book</button>
+        <button>{strings.book}</button>
       </div>
     </div>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,26 @@
 import React, { useState } from "react";
+import LocalizedStrings from "react-localization";
 import Departure from "./Departure";
-
 import "./Home.scss";
+
+let strings = new LocalizedStrings({
+  en: {
+    title: "Find your way to Osheaga",
+    from: "From",
+    to: "To",
+    when: "When",
+    submit: "Search"
+  },
+  fr: {
+    title: "Trouves ton chemin vers Osheaga",
+    from: "Depuis",
+    to: "Vers",
+    when: "Date",
+    submit: "Rechercher"
+  }
+});
+
+const availableLangs = ["en", "fr"];
 
 const formatDeparture = ({ cities, departures }) => {
   return departures.map(
@@ -30,10 +49,10 @@ const formatDeparture = ({ cities, departures }) => {
   );
 };
 
-const DeparturesList = ({ departures }) => (
+const DeparturesList = ({ departures, lang }) => (
   <div className="home__departures">
     {departures.map(departure => (
-      <Departure key={departure.id} {...departure} />
+      <Departure key={departure.id} lang={lang} {...departure} />
     ))}
   </div>
 );
@@ -41,16 +60,22 @@ const DeparturesList = ({ departures }) => (
 const Home = () => {
   const [departures, setDepartures] = useState(null);
   const [error, setError] = useState(null);
+  const [lang, setLang] = useState("en");
+
+  strings.setLanguage(lang);
 
   const getDepartures = e => {
     e.preventDefault();
-    fetch("https://napi.busbud.com/x-departures/dr5reg/f25dvk/2020-08-02", {
-      headers: {
-        Accept:
-          "application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/",
-        "X-Busbud-Token": "PARTNER_AHm3M6clSAOoyJg4KyCg7w"
+    fetch(
+      `https://napi.busbud.com/x-departures/dr5reg/f25dvk/2020-08-02?adult=1&lang=${lang}`,
+      {
+        headers: {
+          Accept:
+            "application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/",
+          "X-Busbud-Token": "PARTNER_AHm3M6clSAOoyJg4KyCg7w"
+        }
       }
-    })
+    )
       .then(res => res.json())
       .then(
         result => {
@@ -63,17 +88,33 @@ const Home = () => {
       );
   };
 
+  const changeLanguage = e => {
+    const lang = e.target.value;
+    setLang(lang);
+  };
+
   return (
     <div className="home">
-      <h1>Find your way to Osheaga</h1>
-      {departures ? (
-        <DeparturesList departures={departures} />
+      <select
+        className="home__languages"
+        onChange={changeLanguage}
+        value={lang}
+      >
+        {availableLangs.map(language => (
+          <option key={language} value={language}>
+            {language.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <h1>{strings.title}</h1>
+      {departures && !!departures.length ? (
+        <DeparturesList departures={departures} lang={lang} />
       ) : (
         <form className="home__search" onSubmit={getDepartures}>
           {error && `Error! ${error.message}`}
           <div className="home__search__fields">
             <div className="home__search__fields__input">
-              <label htmlFor="from">From</label>
+              <label htmlFor="from">{strings.from}</label>
               <input
                 readOnly
                 id="from"
@@ -83,11 +124,11 @@ const Home = () => {
               />
             </div>
             <div className="home__search__fields__input">
-              <label htmlFor="to">To</label>
+              <label htmlFor="to">{strings.to}</label>
               <input readOnly id="to" type="text" name="to" value="Montreal" />
             </div>
             <div className="home__search__fields__input">
-              <label htmlFor="date">When</label>
+              <label htmlFor="date">{strings.when}</label>
               <input
                 readOnly
                 id="when"
@@ -100,7 +141,7 @@ const Home = () => {
           <input
             className="home__search__submit"
             type="submit"
-            value="Search"
+            value={strings.submit}
           />
         </form>
       )}
