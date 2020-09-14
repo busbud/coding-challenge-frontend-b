03deaed835eb05dcb328989c6ae29eaa23f06cdb
Dorine Allali <allali.dorine@gmail.com>
2020-09-13 22:49:48 -0400

    build

---
+++ build/precache-manifest.4e471db5acb4baed419d75529d26e178.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09d4c748f6f34b9569eca455e7a88fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8764503de0fbc97805b1",
+    "url": "/static/css/main.bb51fe69.chunk.css"
+  },
+  {
+    "revision": "2050f32f1208dae45d5a",
+    "url": "/static/js/2.f2390b9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f2390b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8764503de0fbc97805b1",
+    "url": "/static/js/main.61a9adf1.chunk.js"
+  },
+  {
+    "revision": "ce394d7333c218aa2598",
+    "url": "/static/js/runtime-main.ba1c7691.js"
+  },
+  {
+    "revision": "d27b6b12d96d9cf68f493c78113ce390",
+    "url": "/static/media/PlayfairDisplay-Bold.d27b6b12.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "97df1c6468b9ab1d57dc2ce706ecbbfd",
+    "url": "/static/media/bus.97df1c64.svg"
+  },
+  {
+    "revision": "bc61a025215fc25b710ede1b6abe609d",
+    "url": "/static/media/calendar.bc61a025.svg"
+  },
+  {
+    "revision": "4d86e8d9b71c3993a1966e08ea97f98c",
+    "url": "/static/media/edit.4d86e8d9.svg"
+  },
+  {
+    "revision": "84b2d8f871a110a015445caff0b37501",
+    "url": "/static/media/half-arrow-right.84b2d8f8.svg"
+  },
+  {
+    "revision": "0e2c06db21e2f14ee6e584a829bf390d",
+    "url": "/static/media/passengers.0e2c06db.svg"
+  }
+]);
