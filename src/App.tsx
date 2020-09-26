b2d682ac8d1ce0d0a3d108be5021382f1560ad54
claire froelich <claire.froelich@gmail.com>
2020-09-25 20:11:37 -0700

    Force deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edits <code>src/App.tsx</code> and save to reload.
+          Edits2 <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
