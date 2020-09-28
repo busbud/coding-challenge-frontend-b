2914f430f049f2fec9f5da73e443c0ed7453dbf3
Hugo Deschenes <hugo.deschenes2@gmail.com>
2020-09-27 21:35:36 -0400

    Build

---
+++ festival-tracker/app/build/precache-manifest.810dafea9136a61050e84980b4ef2ef4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6c693729f589b83beacae56b203652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81f99e72ac58b35f3046",
+    "url": "/static/css/2.941c1f5b.chunk.css"
+  },
+  {
+    "revision": "8002adb58c6190c49236",
+    "url": "/static/css/main.e00f35f7.chunk.css"
+  },
+  {
+    "revision": "81f99e72ac58b35f3046",
+    "url": "/static/js/2.56240cdf.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.56240cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8002adb58c6190c49236",
+    "url": "/static/js/main.3fb71038.chunk.js"
+  },
+  {
+    "revision": "acdb3d4582248e0e24d0",
+    "url": "/static/js/runtime-main.835adc88.js"
+  },
+  {
+    "revision": "81e1a2235d8a52a5d58d3eca59dea999",
+    "url": "/static/media/osheaga.81e1a223.png"
+  }
+]);
