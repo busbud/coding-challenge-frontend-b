de99133516a4d28fc41b3f1cd5d3f753be24e959
Hugo Deschenes <hugo.deschenes2@gmail.com>
2020-09-27 21:08:34 -0400

    Deploy with nodejs

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const publicPath = path.join(__dirname, '..', 'festival-tracker/app/build')
+console.log(publicPath)
+const port = process.env.PORT || 3000
+app.use(express.static(publicPath))
+
+app.listen(port, () => {
+  console.log('Server is up!')
+})
