84cce97cfd86eeb61c238c64fa40de17d273d155
douglas-pires <douglas.pires@live.com>
2020-10-23 13:47:42 -0300

    Configuring jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '@/': ['./'],
+  },
+};
