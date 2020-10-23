41e2a08a36b6af0e5cf6b1d6c19b9e0057e82acc
douglas-pires <douglas.pires@live.com>
2020-10-23 02:06:46 -0300

    Change Departures component structure

---
+++ components/BudDepartures/BudDepartures.tsx
@@ -0,0 +1,36 @@
+import { Departure } from '@/store/departures/state';
+import BudSkeleton from '@/components/BudSkeleton';
+
+import BudTravelCard from '@/components/BudTravelCard';
+import { Operator } from '@/store/operators/state';
+
+export type BudDeparturesProps = {
+  departures: Departure[];
+  operators: Operator[];
+  loading: boolean;
+};
+
+const BudDepartures: React.FC<BudDeparturesProps> = ({
+  departures,
+  operators,
+  loading,
+}) =>
+  (
+    <div className="mt-8 px-8 pb-8">
+      {loading ? (
+        <BudSkeleton />
+      ) : (
+        departures.map(departure =>
+          (
+            <BudTravelCard
+              key={departure.id}
+              departure={departure}
+              operator={operators.find(op =>
+                op.id === departure.operator_id)}
+            />
+          ))
+      )}
+    </div>
+  );
+
+export default BudDepartures;
