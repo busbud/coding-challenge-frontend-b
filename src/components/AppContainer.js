a02d9995eb6654259d8d2beb67ac104ec428705d
Manuel Torre <manueltorre90@gmail.com>
2020-10-24 15:21:58 -0300

    using react-query to fetch data. basic logic

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -2,12 +2,31 @@ import React from 'react';
 import List from './List'
 import Footer from './Footer'
 import HeaderContainer from './Header/HeaderContainer'
+import { useQuery, QueryCache, ReactQueryCacheProvider } from "react-query";
 
-export default function AppContainer(props){
+const queryCache = new QueryCache();
+
+export default function queryWrapperContainer(props){
+    return(
+        <ReactQueryCacheProvider queryCache={queryCache}>
+            <Container />
+        </ReactQueryCacheProvider>        
+    )
+}
+
+function Container(props){
+
+    const { isLoading, error, data, refetch } = useQuery("repoData", () =>
+    fetch(
+      "https://api.github.com/repos/tannerlinsley/react-query"
+    ).then((res) => res.json())
+    , 
+    {manual: true});    
+    
     return(
         <div>
             <HeaderContainer/>
-            <List/>
+            <List data={data} triggerSearch={refetch}/>
             <Footer/>
         </div>
     )
