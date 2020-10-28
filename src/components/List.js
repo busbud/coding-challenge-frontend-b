2beaa7eacf61317c4d1d14b0bd10c6a3d029eb91
Manuel Torre <manueltorre90@gmail.com>
2020-10-28 00:56:19 -0300

    added localization

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Ticket from './Ticket'
+import { useTranslation } from 'react-i18next';
+
 
 export default function List({latestData, isLoading, error, enabled}){
+
+    const {t} = useTranslation();
+
     const [departures, setDepartures] = useState([]);
     const [operators, setOperators] = useState([]);
     const [cities, setCities] = useState([]);
@@ -27,10 +32,10 @@ export default function List({latestData, isLoading, error, enabled}){
 
     return(
         <div className="list">
-            {error ? <div>There was an error while fetching results. Please, try a different date</div> : ''}
-            {noResults && <div>There are no results or your search</div>}
+            {error ? <div>{t("error")}</div> : ''}
+            {noResults && <div>{t("noResults")}</div>}
             {loadingInitialResults && <img className="loading" src="loader-blue.gif"/>}
-            {pollingMoreResults && <div className="polling"><span>Getting you more</span><img src="loader-blue.gif"/></div>}
+            {pollingMoreResults && <div className="polling"><span>{t("polling")}</span><img src="loader-blue.gif"/></div>}
             {departures.length > 0 && departures.map((departure, index) => {
                 let operator = operators.filter(operators => operators.id === departure.operator_id)[0]
                 return <Ticket key={index} departure={departure} operator={operator} cities={cities}/>
---
+++ src/languages/en.js
@@ -0,0 +1,9 @@
+export default {
+    "title": "Welcome to Osheaga 2020 event!",
+    "subtitle": "Please, take a seat!",
+    "greetings": "We are really pleased to help you finding your best ticket for the Osheaga festival 2020. Please, keep safe and have fun!",
+    "search": "Search",
+    "polling": "Fetching more results",
+    "noResults": "There are no results or your search",
+    "error": "There was an error while fetching results. Please, try a different date"
+  }
