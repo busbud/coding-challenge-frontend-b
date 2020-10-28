7c8ba846e22344175c42b25739a95127f0bfcaab
Manuel Torre <manueltorre90@gmail.com>
2020-10-28 02:20:15 -0300

    fixed image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './styles/App.scss';
+import osheaga from './osheaga.png'
 import AppContainer from './components/AppContainer'
 
 export default function App() {
   return (
     <div className="App">
       <AppContainer/>
-      <a href="https://osheaga.com" target="_blank"><img src={'../osheaga.png'} className="ribbon"/></a>
+      <a href="https://osheaga.com" target="_blank"><img src={osheaga} className="ribbon"/></a>
     </div>
   );
 }
