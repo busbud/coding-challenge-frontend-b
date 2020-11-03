8a036f490bfe0096f765687f7a38138bb97e9664
ferrarisMat <mathieu.f@nextmoov.be>
2020-11-03 02:10:00 +0100

    feat(GlobalStyle): add sectionStyle shared style

--- src/components/GlobalStyle/index.js
+++ src/components/GlobalStyle/index.js
@@ -1,14 +1,26 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
+
+export const sectionStyle = css`
+    width: 80%;
+    padding: 32px 0;
+    @media screen and (max-width: 667px){
+        width: 100%;
+        padding: 0 16px;
+        box-sizing: border-box;
+    }
+    margin: 0 auto;
+`;
 
 const GlobalStyle = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.colors.primary};
     color: ${({ theme }) => theme.colors.secondary};
     transition: background .4s ease-in-out, color .4s ease-in-out;
-    a {
-      text-decoration: none;
-      color: ${({ theme }) => theme.colors.highlight};
-    }
+    font-family: sans-serif;
+    margin: 0;
+  }
+  #root {
+    background-image: ${({ theme }) => theme.colors.mainGradient};
   }
 `;
 
