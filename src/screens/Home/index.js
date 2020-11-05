2a7f2d9d3b3e8e81de69d13d9786ce02b619d743
ferrarisMat <mathieu.f@nextmoov.be>
2020-11-06 00:26:06 +0100

    feat(Home): add call to action banner and hero image

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,18 +1,32 @@
 // @flow
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { isMobile } from 'react-device-detect';
 import ArtistsSection from '../../containers/ArtistsSection';
-import { HomeTitle } from './styledComp';
+import { HomeTitle, HomeWrapper } from './styledComponent';
+import Banner from '../../components/Banner';
+import logoSrc from '../../assets/images/Logo-Osheaga-FR-01.png';
 
 function HomeScreen() {
   const { t } = useTranslation();
   return (
-    <>
+    <HomeWrapper>
+      <img src={logoSrc} alt="Osheaga Festival" />
       <HomeTitle>
         {t('home_title')}
       </HomeTitle>
       <ArtistsSection />
-    </>
+      <Banner
+        title={t('home_banner_cta_title')}
+        content={t('home_banner_cta_content')}
+        type="highlight"
+        fixedTo="bottom"
+        closable={isMobile}
+        linkContent={t('home_banner_cta')}
+        linkTo="/travel-tickets"
+        textAlign="right"
+      />
+    </HomeWrapper>
   );
 }
 
