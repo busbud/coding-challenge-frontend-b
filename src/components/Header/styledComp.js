0bdc86ec2ee34101f84168218eaad97be5039d09
ferrarisMat <mathieu.f@nextmoov.be>
2020-11-06 00:33:05 +0100

    feat(Header): add media queries

--- src/components/Header/styledComp.js
+++ src/components/Header/styledComp.js
@@ -7,6 +7,11 @@ export const HeaderWrapper = styled.div`
     align-items: center;
     padding: 32px;
     box-sizing: border-box;
+    @media screen and (max-width: 667px){
+        flex-direction: column;
+        align-items: flex-start;
+        position: sticky;
+    }
 `;
 
 const childSpacing = (spacing = 8) => css`
@@ -36,6 +41,12 @@ export const LangWrapper = styled.div`
     display: flex;
     font-size: 18px;
     align-items: center;
+    
+    @media screen and (max-width: 667px){
+        font-size: 16px;
+        align-self: flex-end;
+    }
+
     button {
         cursor: pointer;
         ${childSpacing}
@@ -54,6 +65,7 @@ export const LangWrapper = styled.div`
             opacity: 1;
         }
     }
+
     .active {
         opacity: 1;
         ${itemAfterHover}
@@ -66,6 +78,10 @@ export const LangWrapper = styled.div`
 export const RoutesWrapper = styled.div`
     display: flex;
     align-items: center;
+    @media screen and (max-width: 667px){
+        width: 100%;
+        justify-content: space-between;
+    }
     a {
         font-size: 24px;
         text-decoration: none;
@@ -73,6 +89,14 @@ export const RoutesWrapper = styled.div`
         font-weight: bold;
         ${itemAfterHover}
         color: ${({ theme }) => theme.colors.secondary};
+        @media screen and (max-width: 667px){
+            font-size: 18px;
+            ${childSpacing(16)}
+            svg {
+                width: 150px;
+            }
+        }
+
         h1 {
             text-indent: -10000px;
             position: absolute;
