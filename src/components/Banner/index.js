4b6308da25298deb6933ef20e55ee7a6e06bc069
ferrarisMat <mathieu.f@nextmoov.be>
2020-11-06 00:32:51 +0100

    feat(Banner): add type and support for fixed and link

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { BannerWrapper } from './styledComponent';
-// import { ReactComponent as CloseIcon } from '../../assets/images/cross-icon.svg';
+import { Link } from 'react-router-dom';
+import { BannerWrapper, ContentWrapper } from './styledComponent';
 
 type Props = {
     title: String, // eslint-disable-line
     content: String, // eslint-disable-line
-    closable?: Boolean, //eslint-disable-line
+    closable?: Boolean,
     onCloseClick?: Function,
+    type: String,
+    linkContent?: String, //eslint-disable-line
+    linkTo?: String, //eslint-disable-line
+    fixedTo?: 'bottom' | 'top', //eslint-disable-line
+    textAlign?: 'center' | 'left' | 'right', //eslint-disable-line
 }
 
 function Banner({
@@ -14,34 +19,52 @@ function Banner({
   content,
   closable,
   onCloseClick,
+  type,
+  linkContent,
+  linkTo,
+  fixedTo,
+  textAlign,
 }: Props) {
   const [showBanner, setShowBanner] = useState(true);
+
   return (
     <>
       {showBanner && (
-        <BannerWrapper>
+        <BannerWrapper
+          type={type}
+          fixedTo={fixedTo}
+          textAlign={textAlign}
+        >
           {(title || content) && (
-          <div>
-            {title && (
-            <h4>
-              {title}
-            </h4>
-            )}
-            {content && (
-            <p>
-              {content}
-            </p>
-            )}
-          </div>
+            <ContentWrapper type={type}>
+              {title && (
+                <h4>
+                  {title}
+                </h4>
+              )}
+              {content && (
+                <p>
+                  {content}
+                </p>
+              )}
+            </ContentWrapper>
           )}
-          {(closable) && (
-            <button
-              type="button"
-              onClick={(e) => { onCloseClick(e); setShowBanner(false); }}
-            >
-              close
-              {/* <CloseIcon /> */}
-            </button>
+          {(closable || (linkTo && linkContent)) && (
+            <div style={{ display: 'flex' }}>
+              {(linkTo && linkContent) && (
+                <Link to={linkTo}>
+                  {linkContent}
+                </Link>
+              )}
+              {closable && (
+                <button
+                  type="button"
+                  onClick={(e) => { onCloseClick(e); setShowBanner(false); }}
+                >
+                  close
+                </button>
+              )}
+            </div>
           )}
         </BannerWrapper>
       )}
@@ -51,6 +74,8 @@ function Banner({
 
 Banner.defaultProps = {
   onCloseClick: () => { },
+  closable: false,
+  textAlign: 'left',
 };
 
 export default Banner;
