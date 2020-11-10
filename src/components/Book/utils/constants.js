463ff27744375acb8d928bb59d37fbf3e02c6f75
SiamakEshghi <si.eshghi@gmail.com>
2020-11-10 14:51:06 -0500

    added constants folder for keeping data for making url

---
+++ src/components/Book/utils/constants.js
@@ -0,0 +1,5 @@
+const origin = 'f2m673';
+const destination = 'f25dvk';
+const outbound_date = '2020-12-17';
+
+export const url = `https://napi.busbud.com/x-departures/${origin}/${destination}/${outbound_date}`;
