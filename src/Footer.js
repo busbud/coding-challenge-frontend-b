b5396a2caca221b56376e5ab5179fbf94d7cf3f1
anish8600 <37567093+anish8600@users.noreply.github.com>
2020-11-18 19:20:42 -0500

    Update Footer.js
    Removing offset from Footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
       <div className="row">
-        <div className="col-12 offset-2">
+        <div className="col-12">
           <img src="/logos/ReduxLogo.png" />
                Contact us: joseph.20.lawrence@gmail.com
         </div>
