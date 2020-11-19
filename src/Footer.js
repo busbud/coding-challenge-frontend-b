5d4f27e5d0053319c63abf48ee6c1489dd66eaaa
anish8600 <37567093+anish8600@users.noreply.github.com>
2020-11-18 20:25:09 -0500

    Update Footer.js
    Added css class in footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
       <div className="row">
-        <div className="col-12">
+        <div className="col-12 section-spacing">
           <img src="/logos/ReduxLogo.png" />
                Contact us: joseph.20.lawrence@gmail.com
         </div>
