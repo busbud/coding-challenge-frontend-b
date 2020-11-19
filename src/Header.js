0fad129199b377a8b452b8c956ebc08fbb8b48f5
anish8600 <37567093+anish8600@users.noreply.github.com>
2020-11-18 20:24:43 -0500

    Update Header.js
    Added css class

--- src/Header.js
+++ src/Header.js
@@ -17,7 +17,7 @@ const Header = () => (
             </div>
         </div>
       <div className="row">
-        <div className="col-12">
+        <div className="col-12 section-spacing">
           <img src="/logos/ReduxLogo.png" />
                Online Bus Booking System
         </div>
