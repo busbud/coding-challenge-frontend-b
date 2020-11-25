5437b139867dd3e8c56b552e85d28cbb9e1317be
Slim <slim.khadhraoui@gmail.com>
2020-11-25 16:21:55 -0500

    Added a client and configured webpack Added the required dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'client', 'src', 'index.tsx')
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'client/dist')
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 300000,
+        maxAssetSize: 300000
+    },
+}
