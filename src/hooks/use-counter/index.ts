c1dbff6f448e7478d936a90addee202d04cf8fff
Marcus Silva <mvfsillva@gmail.com>
2021-01-15 14:48:11 -0300

    feat: add use-counter hook

---
+++ src/hooks/use-counter/index.ts
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+type Params = {
+  initialCount?: number
+  step?: number
+}
+
+function useCounter({ initialCount = 0, step = 1 }: Params = {}) {
+  const [count, setCount] = useState(initialCount)
+
+  const increment = () => setCount((c) => c + step)
+  const decrement = () => setCount((c) => c - step)
+
+  return { count, increment, decrement }
+}
+
+export default useCounter
