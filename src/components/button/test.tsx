848d442c0841ee2b6b239d1e15f788a4008e0ba4
Marcus Silva <mvfsillva@gmail.com>
2021-01-15 01:32:52 -0300

    feat: add button component

---
+++ src/components/button/test.tsx
@@ -0,0 +1,41 @@
+// Packages
+import React from 'react'
+import { fireEvent, cleanup } from '@testing-library/react'
+
+// Helpers
+import { renderWithTheme } from 'helpers/testing-library'
+
+// Components
+import Button from '.'
+
+afterEach(cleanup)
+
+const handleClick = jest.fn()
+describe('<Button />', () => {
+  it('should render a button', () => {
+    const { getByTestId } = renderWithTheme(
+      <Button onClick={handleClick}>Click Me</Button>
+    )
+
+    expect(getByTestId(/button/)).toBeInTheDocument()
+  })
+
+  it('should call a function after clicking on the button', () => {
+    const { getByTestId } = renderWithTheme(
+      <Button onClick={handleClick}>Click Me</Button>
+    )
+
+    fireEvent.click(getByTestId(/button/))
+    expect(handleClick).toBeCalled()
+  })
+
+  it('should render the button colors correctly', () => {
+    const { container } = renderWithTheme(
+      <Button primary onClick={handleClick}>
+        Click Me
+      </Button>
+    )
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#FBAE16' })
+  })
+})
