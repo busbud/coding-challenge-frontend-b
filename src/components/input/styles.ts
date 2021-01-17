1af866b5c207f530a42dd9fb1a0b662212f12274
Marcus Silva <mvfsillva@gmail.com>
2021-01-17 05:40:17 -0300

    refactor: improve input style

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -4,31 +4,35 @@ import styled, { css } from 'styled-components'
 import { rgba } from 'polished'
 
 export const Container = styled.div`
-  ${({ theme }) => css`
-    > input {
-      padding-left: calc(${theme.spaces.small} - 4px);
-    }
-  `}
+  > input {
+    padding-left: 4.2rem;
+  }
 `
 
 export const Icon = styled.div`
-  position: absolute;
-  padding: 12px;
-  pointer-events: none;
+  ${({ theme }) => css`
+    position: absolute;
+    padding: 18px;
+    pointer-events: none;
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.gray};
+  `}
 `
 
 export const Field = styled.input<{ disabled?: boolean; icon?: ReactElement }>`
   ${({ theme, disabled }) => css`
+    width: 100%;
+    min-height: 36px;
     background-color: ${disabled && theme.colors.muted};
     border-radius: ${theme.border.radius};
     border-width: 0px;
     bottom: 0px;
-    box-shadow: 0 0 0 1px ${rgba(theme.colors.gray, 1)};
+    box-shadow: 0 0 0 1px ${rgba(theme.colors.muted, 1)};
     display: block;
-    font-size: calc(${theme.font.sizes.base} - 1px);
+    font-size: ${theme.font.sizes.small};
     outline: 0;
-    color: ${theme.colors.darkGray};
-    padding: ${theme.spaces.tiny};
+    color: ${theme.colors.gray};
+    padding: 1.7rem;
 
     ::placeholder {
       color: ${theme.colors.darkGray};
