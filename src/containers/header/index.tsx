a00cca70db0a88035892caa698309981e2983476
Marcus Silva <mvfsillva@gmail.com>
2021-01-17 02:01:33 -0300

    feat: add header component

---
+++ src/containers/header/index.tsx
@@ -0,0 +1,15 @@
+// Packages
+import React from 'react'
+
+// Styles
+import * as S from './styles'
+
+const Header = () => {
+  return (
+    <S.Wrapper>
+      <S.Logo src="/img/logo.svg" alt="Busbud" />
+    </S.Wrapper>
+  )
+}
+
+export default Header
