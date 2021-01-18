975c6b4c3458695bd50858a2093b05928a27a9f9
Marcus Silva <mvfsillva@gmail.com>
2021-01-18 12:44:18 -0300

    refactor: remove unncessary code

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
 const withPWA = require('next-pwa')
-
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPlugins([
-  [
-    withPWA,
-    {
-      pwa: {
-        dest: 'public',
-        disable: !isProd
-      }
-    }
-  ],
-  [optimizedImages]
-])
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
