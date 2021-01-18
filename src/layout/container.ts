a064fb836dc2cab8fe5d627aad5f143098c6c8bf
Marcus Silva <mvfsillva@gmail.com>
2021-01-18 03:46:33 -0300

    styles: update spacings

--- src/layout/container.ts
+++ src/layout/container.ts
@@ -4,10 +4,11 @@ import media from 'styled-media-query'
 export const Container = styled.div`
   ${({ theme }) => css`
     max-width: ${theme.grid.container};
-    padding: ${theme.spaces.large} calc(${theme.grid.gutter} / 2);
+    padding: ${theme.spaces.xxsmall} calc(${theme.grid.gutter} / 2);
     margin: 0 auto;
+
     ${media.greaterThan('medium')`
-      padding: ${theme.spaces.large} calc(${theme.grid.gutter} / 2);
+      padding: ${theme.spaces.small} calc(${theme.grid.gutter} / 2);
     `}
   `}
 `
