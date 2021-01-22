8f82a78e4a47defb09c20cbf43255fddd30ac005
Abner S. Alves Jr <abnersajr@gmail.com>
2021-01-22 01:34:03 -0300

    adds unit tests config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,21 @@
 module.exports = {
+  collectCoverage: true,
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        esModuleInterop: true,
+        jsx: 'react',
+      },
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/test-support/setup-tests.ts'],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'identity-obj-proxy',
+  },
 }
