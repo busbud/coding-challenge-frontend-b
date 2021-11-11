3fc4670cfcc9f348b44f6a19c4973acad62ca3d2
Yuri Toledo <yuriwtoledo@gmail.com>
2021-11-11 17:02:59 -0300

    chore: add typescript and eslint

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import type { AppProps /* , AppContext */ } from 'next/app'
+
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
