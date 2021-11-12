8a711aa8ec94ef17174ce15baedc195e9152ba67
Yuri Toledo <yuriwtoledo@gmail.com>
2021-11-12 17:35:38 -0300

    feat: creating LoadingBus component

---
+++ components/LoadingBus.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+const LoadingBus = () => (
+  <Image
+    src="https://i.pinimg.com/originals/40/b0/eb/40b0eb6ebf29df527a0c080cc1227f6b.gif"
+    alt="moving bus"
+  />
+)
+
+export default LoadingBus
