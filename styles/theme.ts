0ef807d44260a756f2b6bde9d9e4ffa6e5aef306
Yuri Toledo <yuriwtoledo@gmail.com>
2021-11-12 17:36:28 -0300

    feat: create theme file

---
+++ styles/theme.ts
@@ -0,0 +1,20 @@
+export enum colors {
+  main = '#DF0B9E',
+  default = '#111111',
+}
+
+export enum screenSize {
+  small = 640,
+  medium = 1007,
+}
+
+export enum fontSize {
+  md = 16,
+  lg = 24,
+}
+
+export const theme = {
+  colors,
+  fontSize,
+  screenSize,
+}
