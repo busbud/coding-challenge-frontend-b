307ea6120508141c0d8605975aacf68db8457077
Yuri Toledo <yuriwtoledo@gmail.com>
2021-11-16 20:49:23 -0300

    feat: improve UX with status messages

--- components/DestinationList/DestinationList.tsx
+++ components/DestinationList/DestinationList.tsx
@@ -1,19 +1,24 @@
 import { FC } from 'react'
 import { useDestination } from '../../hooks/useDestination'
+import { FetchStatus } from '../../interfaces'
 import Card from '../Card/Card'
+import ErrorListStatus from '../ErrorListStatus/ErrorListStatus'
+import InitialListStatus from '../InitialListStatus/InitialListStatus'
 import LoadingBus from '../LoadingBus/LoadingBus'
 import { List } from './styles'
 
 const DestinationList: FC = () => {
-  const { destinationList, isFetching } = useDestination()
+  const { destinationList, fetchingStatus } = useDestination()
+
+  if (fetchingStatus === FetchStatus.initial) return <InitialListStatus />
+  if (fetchingStatus === FetchStatus.error) return <ErrorListStatus />
+  if (fetchingStatus === FetchStatus.loading) return <LoadingBus />
 
   return (
     <List>
-      {isFetching ? (
-        <LoadingBus />
-      ) : (
-        destinationList.map(item => <Card item={item} />)
-      )}
+      {destinationList.map(item => (
+        <Card item={item} />
+      ))}
     </List>
   )
 }
