b0b95482fbfd6bbcdc9aec17998f7d2b9f4ae3cb
Yuri Toledo <yuriwtoledo@gmail.com>
2021-11-16 20:21:47 -0300

    feat: moving head to own file

---
+++ components/Head/Head.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+const HtmlHead = () => (
+  <Head>
+    <title>BuzzBud</title>
+    <meta name="description" content="Proudly create in 🇧🇷 with 🍺" />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+)
+
+export default HtmlHead
