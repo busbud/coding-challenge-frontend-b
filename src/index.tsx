6f813c85887ff287ec476671f37d89893c370720
Adrien Breil <adrien.breil@phantombuster.com>
2021-11-17 01:38:09 +0100

    fix: purge heroku cache

--- src/index.tsx
+++ src/index.tsx
@@ -18,6 +18,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// eslint-disable-next-line no-console
+console.log(process.env);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
