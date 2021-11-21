6a76fd46cef04f02f7657bf40716033d710999f0
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2021-11-21 18:11:21 +0100

    style: update form items grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import Container from "@mui/material/Container";
 import "./App.css";
 import Form from "./Form";
 
 function App() {
   return (
-    <div className="App">
-      <h1>busbud coding challenge</h1>
-
-      <div>
-        <Form />
+    <Container>
+      <div className="App">
+        <h1>busbud coding challenge</h1>
+        <div>
+          <Form />
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
--- src/Form.tsx
+++ src/Form.tsx
@@ -3,8 +3,10 @@ import DatePicker from "@mui/lab/DatePicker";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import Autocomplete from "@mui/material/Autocomplete";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
-import { useState } from "react";
+import { Box } from "@mui/system";
+import { ReactChildren, ReactNode, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { City } from "./types";
 
@@ -19,58 +21,89 @@ const cities: City[] = [
   },
 ];
 
+function FormItem({ children }: { children: ReactNode }) {
+  return (
+    <Box
+      sx={{
+        marginBottom: 5,
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
 export default function Form() {
   const { t } = useTranslation();
   const [date, setDate] = useState<Date | null>(new Date(2021, 7, 2));
 
   return (
-    <>
-      <Autocomplete
-        disablePortal
-        id="departure"
-        options={cities}
-        sx={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField {...params} label={t("Departure")} />
-        )}
-        getOptionLabel={(option) => option.name}
-        defaultValue={{
-          name: "Quebec",
-          geohash: "f2m673",
-        }}
-      />
-      <Autocomplete
-        disablePortal
-        id="arrival"
-        options={cities}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label={t("Arrival")} />}
-        getOptionLabel={(option) => option.name}
-        defaultValue={{
-          name: "Montreal",
-          geohash: "f25dvk",
-        }}
-      />
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DatePicker
-          label={t("Date")}
-          value={date}
-          onChange={(date) => {
-            setDate(date);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </LocalizationProvider>
-      <TextField
-        id="passengers-number"
-        label={t("Passengers")}
-        type="number"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        defaultValue={1}
-      />
+    <Grid container>
+      <Grid item xs={4}>
+        <FormItem>
+          <Autocomplete
+            disablePortal
+            id="departure"
+            options={cities}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField {...params} label={t("Departure")} />
+            )}
+            getOptionLabel={(option) => option.name}
+            defaultValue={{
+              name: "Quebec",
+              geohash: "f2m673",
+            }}
+          />
+        </FormItem>
+      </Grid>
+      <Grid item xs={4}>
+        <FormItem>
+          <Autocomplete
+            disablePortal
+            id="arrival"
+            options={cities}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField {...params} label={t("Arrival")} />
+            )}
+            getOptionLabel={(option) => option.name}
+            defaultValue={{
+              name: "Montreal",
+              geohash: "f25dvk",
+            }}
+          />
+        </FormItem>
+      </Grid>
+      <Grid item xs={4}></Grid>
+      <Grid item xs={4}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <FormItem>
+            <DatePicker
+              label={t("Date")}
+              value={date}
+              onChange={(date) => {
+                setDate(date);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </FormItem>
+        </LocalizationProvider>
+      </Grid>
+      <Grid item xs={4}>
+        <FormItem>
+          <TextField
+            id="passengers-number"
+            label={t("Passengers")}
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            defaultValue={1}
+          />
+        </FormItem>
+      </Grid>
       <Button variant="contained">{t("Search")}</Button>
-    </>
+    </Grid>
   );
 }
