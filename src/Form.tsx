ec85661bc4511075a65632cecdc2f4b21114675e
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2021-11-23 18:57:35 +0100

    style: improve form grid

--- src/Form.tsx
+++ src/Form.tsx
@@ -6,7 +6,7 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import { Box } from "@mui/system";
-import { ReactChildren, ReactNode, useState } from "react";
+import { ReactNode, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { City } from "./types";
 
@@ -21,89 +21,71 @@ const cities: City[] = [
   },
 ];
 
-function FormItem({ children }: { children: ReactNode }) {
-  return (
-    <Box
-      sx={{
-        marginBottom: 5,
-      }}
-    >
-      {children}
-    </Box>
-  );
-}
-
 export default function Form() {
   const { t } = useTranslation();
   const [date, setDate] = useState<Date | null>(new Date(2021, 7, 2));
 
   return (
-    <Grid container>
-      <Grid item xs={4}>
-        <FormItem>
-          <Autocomplete
-            disablePortal
-            id="departure"
-            options={cities}
-            sx={{ width: 300 }}
-            renderInput={(params) => (
-              <TextField {...params} label={t("Departure")} />
-            )}
-            getOptionLabel={(option) => option.name}
-            defaultValue={{
-              name: "Quebec",
-              geohash: "f2m673",
-            }}
-          />
-        </FormItem>
+    <Grid container spacing={2}>
+      <Grid item xs={3}>
+        <Autocomplete
+          disablePortal
+          id="departure"
+          options={cities}
+          renderInput={(params) => (
+            <TextField {...params} label={t("Departure")} />
+          )}
+          getOptionLabel={(option) => option.name}
+          defaultValue={{
+            name: "Quebec",
+            geohash: "f2m673",
+          }}
+          fullWidth
+        />
       </Grid>
-      <Grid item xs={4}>
-        <FormItem>
-          <Autocomplete
-            disablePortal
-            id="arrival"
-            options={cities}
-            sx={{ width: 300 }}
-            renderInput={(params) => (
-              <TextField {...params} label={t("Arrival")} />
-            )}
-            getOptionLabel={(option) => option.name}
-            defaultValue={{
-              name: "Montreal",
-              geohash: "f25dvk",
-            }}
-          />
-        </FormItem>
+      <Grid item xs={3}>
+        <Autocomplete
+          disablePortal
+          id="arrival"
+          options={cities}
+          renderInput={(params) => (
+            <TextField {...params} label={t("Arrival")} />
+          )}
+          getOptionLabel={(option) => option.name}
+          defaultValue={{
+            name: "Montreal",
+            geohash: "f25dvk",
+          }}
+        />
       </Grid>
-      <Grid item xs={4}></Grid>
-      <Grid item xs={4}>
+      <Grid item xs={2}>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <FormItem>
-            <DatePicker
-              label={t("Date")}
-              value={date}
-              onChange={(date) => {
-                setDate(date);
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </FormItem>
+          <DatePicker
+            label={t("Date")}
+            value={date}
+            onChange={(date) => {
+              setDate(date);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
         </LocalizationProvider>
       </Grid>
-      <Grid item xs={4}>
-        <FormItem>
-          <TextField
-            id="passengers-number"
-            label={t("Passengers")}
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            defaultValue={1}
-          />
-        </FormItem>
+      <Grid item xs={2}>
+        <TextField
+          id="passengers-number"
+          label={t("Passengers")}
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          defaultValue={1}
+        />
       </Grid>
-      <Button variant="contained">{t("Search")}</Button>
+      <Grid item xs={2}>
+        <Button variant="contained" size="large">
+          {t("Search")}
+        </Button>
+      </Grid>
     </Grid>
   );
 }
