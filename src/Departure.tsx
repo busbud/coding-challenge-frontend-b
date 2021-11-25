486aa0ee5cb57cb790c6d857db587c7fa659dd9b
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2021-11-25 23:26:50 +0100

    style: add basic styling to departure

--- src/Departure.tsx
+++ src/Departure.tsx
@@ -1,6 +1,8 @@
 import format from "date-fns/format";
 import parseISO from "date-fns/parseISO";
 import { useTranslation } from "react-i18next";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 interface Props {
   departureTime: string;
@@ -13,19 +15,21 @@ export default function Departure(props: Props) {
   const { t } = useTranslation();
 
   return (
-    <div>
-      <div>
-        {t("Departure time: {{date}}", {
-          date: format(parseISO(props.departureTime), "PPpp"),
-        })}
-      </div>
-      <div>
-        {t("Arrival time: {{date}}", {
-          date: format(parseISO(props.arrivalTime), "PPpp"),
-        })}
-      </div>
-      <div>{t("Location: {{location}}", { location: props.location })}</div>
-      <div>{t("Price: {{price}}", { price: props.price })}</div>
-    </div>
+    <Card>
+      <CardContent>
+        <div>
+          {t("Departure time: {{date}}", {
+            date: format(parseISO(props.departureTime), "PPpp"),
+          })}
+        </div>
+        <div>
+          {t("Arrival time: {{date}}", {
+            date: format(parseISO(props.arrivalTime), "PPpp"),
+          })}
+        </div>
+        <div>{t("Location: {{location}}", { location: props.location })}</div>
+        <div>{t("Price: {{price}}", { price: props.price })}</div>
+      </CardContent>
+    </Card>
   );
 }
