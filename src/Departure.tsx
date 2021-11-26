217248da1c08e3ef67b9c72c9bc010bc18dcf31f
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2021-11-26 09:37:22 +0100

    style: update price display style

--- src/Departure.tsx
+++ src/Departure.tsx
@@ -3,6 +3,9 @@ import parseISO from "date-fns/parseISO";
 import { useTranslation } from "react-i18next";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import Box from "@mui/system/Box";
 
 interface Props {
   departureTime: string;
@@ -15,20 +18,31 @@ export default function Departure(props: Props) {
   const { t } = useTranslation();
 
   return (
-    <Card>
+    <Card sx={{ marginBottom: 2 }}>
       <CardContent>
-        <div>
-          {t("Departure time: {{date}}", {
-            date: format(parseISO(props.departureTime), "PPpp"),
-          })}
-        </div>
-        <div>
-          {t("Arrival time: {{date}}", {
-            date: format(parseISO(props.arrivalTime), "PPpp"),
-          })}
-        </div>
-        <div>{t("Location: {{location}}", { location: props.location })}</div>
-        <div>{t("Price: {{price}}", { price: props.price })}</div>
+        <Grid container>
+          <Grid item flexGrow={3} textAlign="left">
+            <div>
+              {t("Departure time: {{date}}", {
+                date: format(parseISO(props.departureTime), "PPpp"),
+              })}
+            </div>
+            <div>
+              {t("Arrival time: {{date}}", {
+                date: format(parseISO(props.arrivalTime), "PPpp"),
+              })}
+            </div>
+            <div>
+              {t("Location: {{location}}", { location: props.location })}
+            </div>
+          </Grid>
+          <Divider orientation="vertical" flexItem />
+          <Grid item flexGrow={1}>
+            <Box sx={{ fontSize: "2rem", fontWeight: 600 }}>
+              {t("Price: {{price}}", { price: props.price })}
+            </Box>
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
