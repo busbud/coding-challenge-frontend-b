274c9f8a49475ef71f6636bf1abb57f06836d284
Philippe <philippe2008@hotmail.com>
2022-07-02 00:28:05 -0400

    Update initial pages.

--- departures/src/index.tsx
+++ departures/src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Home from './Pages/Home';
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Home />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
