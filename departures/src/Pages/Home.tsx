e56f990bb3026b76184d3c67dd4ece28c402bf10
Philippe <philippe2008@hotmail.com>
2022-07-03 10:33:47 -0400

    Formatting departure card.

--- departures/src/Pages/Home.tsx
+++ departures/src/Pages/Home.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import { Alert, Container, Row, Col } from 'react-bootstrap';
 
+import moment from 'moment';
+
 import { fetchDepartures, TQueryParams } from '../Connections/connections';
 
 import { BusbudLogo } from '../Components/Icons';
@@ -28,7 +30,8 @@ interface TDeparture {
     arrivalTime: string,
     locationName_Origin: string,
     locationName_Destination: string,
-    price: number
+    price: number,
+    currency: string
 }
 
 const Home = () => {
@@ -40,7 +43,7 @@ const Home = () => {
         const _locations = data.locations;
         const _cities = data.cities;
 
-        //console.log('DATA ', data);
+        console.log('DATA ', data);
 
         const _departures: Array<TDeparture> = data.departures.map((item: any) => {
 
@@ -59,7 +62,8 @@ const Home = () => {
                 arrivalTime: item.arrival_time,
                 locationName_Origin,
                 locationName_Destination,
-                price: item.prices.total
+                price: item.prices.total,
+                currency: item.prices.currency
             }
         });
 
@@ -92,20 +96,31 @@ const Alerts = () => {
     )
 }
 
+const FormatDate = (dateString: string) => {
+    const date = new Date(dateString);
+
+    const time = moment(date).format('HH:mm');
+
+    return time;
+}
+
 interface DepartureCardProps {
     departure: TDeparture
 }
 const DepartureCard = (props: DepartureCardProps) => {
-    const { departureTime, arrivalTime, locationName_Origin, locationName_Destination, price } = props.departure;
+    const { departureTime, arrivalTime, locationName_Origin, locationName_Destination, price, currency } = props.departure;
+
+    const _departureTime = FormatDate(departureTime);
+    const _arrivalTime = FormatDate(arrivalTime);
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'row', backgroundColor: 'beige', margin: '4px' }}>
+        <div style={{ display: 'flex', flexDirection: 'row', width: '100%', backgroundColor: 'cyan', margin: '4px', borderRadius: '4px' }}>
             <div style={{ flexGrow: 1 }}>
-                <div>{departureTime} - {locationName_Origin}</div>
-                <div>{arrivalTime} - {locationName_Destination}</div>
+                <div>{_departureTime} - {locationName_Origin}</div>
+                <div>{_arrivalTime} - {locationName_Destination}</div>
             </div>
-            <div style={{ width: '120px' }}>
-                <div>{price}</div>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <div>{currency}${price}</div>
             </div>
         </div>
     )
@@ -125,11 +140,13 @@ const Form = (departures: Array<TDeparture>) => {
                 </Col>
             </Row>
             <Row>
-                <Col>
+                <Col sm={3}></Col>
+                <Col sm={6}>
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                         {departures.map((departure) => <DepartureCard key={departure.id} departure={departure} />)}
                     </div>
                 </Col>
+                <Col sm={3}></Col>
             </Row>
             {/* <Row>
                 <Col sm>sm=true</Col>
